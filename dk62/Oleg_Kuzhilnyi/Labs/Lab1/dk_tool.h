1cfa289f4725e45ce6d48d61b8ceeb6da03818b9
Jayawardenepura <kuzhilnyioleg16@gmail.com>
2017-02-23 14:38:34 +0200

    Lab_One
    Variant №4

---
+++ dk62/Oleg_Kuzhilnyi/Labs/Lab1/dk_tool.h
@@ -0,0 +1,14 @@
+//
+//  dk_tool.h
+//  labs_KPI
+//
+//  Created by air on 23.02.17.
+//  Copyright © 2017 airMair. All rights reserved.
+//
+
+#ifndef dk_tool_h
+#define dk_tool_h
+
+#include <stdio.h>
+
+#endif /* dk_tool_h */
