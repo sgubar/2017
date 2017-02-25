539bf294b4dac50c02a15e808da808d758b7b64c
dest1nyd1spers1on <alexafa@ukr.net>
2017-02-25 15:53:53 +0200

    Update dk_tools.c

--- dk62/Alex1s/laba1/dk_tools.c
+++ dk62/Alex1s/laba1/dk_tools.c
@@ -8,18 +8,18 @@ if(d == 0)
 	}
 
 int i = 0;
-unsigned long int result = 1;
+unsigned long int result = 1;    //can be a very large number
 
 for(i = 1; i <= d; i++)
     {
-    result = result * i;
+    result *= i;                 //result = result * i;
     }
 return result;
 }
 
-double drob(int a, int b, int c)
+double drob(int a, int b, int c)  //the first part of the formula
 {
-if(b+c*c == 0)
+if(b+c*c == 0)                    //division by zero
 	{
 	printf("Error!\n");
 	return 1;
