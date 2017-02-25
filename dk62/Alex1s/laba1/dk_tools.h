08f107d43398dd86f39cf28367b3b3442b94ef28
dest1nyd1spers1on <alexafa@ukr.net>
2017-02-25 15:45:03 +0200

    Add files via upload

---
+++ dk62/Alex1s/laba1/dk_tools.h
@@ -0,0 +1,7 @@
+#ifndef dk_tool_h
+#define dk_tool_h
+
+unsigned long int factorial(int d);
+double drob(int a, int b, int c);
+
+#endif
---
+++ dk62/Alex1s/laba1/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dk_tools.h"
+
+int main(int argc, char *argv[]) {
+int a = 0, b = 0, c = 0, d = 0;
+double q = 0;
+printf("Enter A: ");
+scanf("%d", &a);
+printf("Enter B: ");
+scanf("%d", &b);
+printf("Enter C: ");
+scanf("%d", &c);
+
+q = drob(a, b, c);
+for(d = 0; d <= b; d++)
+q = q + factorial(d);
+
+printf("Q = %.6lf\n", q);
+
+return 0;
+}
