ac73e060da329bc2ada9ce7c592c21f4814845ff
dest1nyd1spers1on <alexafa@ukr.net>
2017-02-25 15:55:34 +0200

    Update dk_tools.c

--- dk62/Alex1s/laba1/dk_tools.c
+++ dk62/Alex1s/laba1/dk_tools.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-unsigned long int factorial(int d) {
+unsigned long int factorial(int d) //the second part of the formula
+{
 if(d == 0)
 	{
 	int h = 1;
@@ -8,18 +9,18 @@ if(d == 0)
 	}
 
 int i = 0;
-unsigned long int result = 1;    //can be a very large number
+unsigned long int result = 1;      //can be a very large number
 
 for(i = 1; i <= d; i++)
     {
-    result *= i;                 //result = result * i;
+    result *= i;                   //result = result * i;
     }
 return result;
 }
 
-double drob(int a, int b, int c)  //the first part of the formula
+double drob(int a, int b, int c)   //the first part of the formula
 {
-if(b+c*c == 0)                    //division by zero
+if(b+c*c == 0)                     //division by zero
 	{
 	printf("Error!\n");
 	return 1;
