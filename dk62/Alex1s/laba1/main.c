75bec23d71f683a64b3dc530d12e0eeb5045d446
dest1nyd1spers1on <alexafa@ukr.net>
2017-02-25 15:57:13 +0200

    Update main.c

--- dk62/Alex1s/laba1/main.c
+++ dk62/Alex1s/laba1/main.c
@@ -5,6 +5,7 @@
 int main(int argc, char *argv[]) {
 int a = 0, b = 0, c = 0, d = 0;
 double q = 0;
+  
 printf("Enter A: ");
 scanf("%d", &a);
 printf("Enter B: ");
@@ -12,11 +13,11 @@ scanf("%d", &b);
 printf("Enter C: ");
 scanf("%d", &c);
 
-q = drob(a, b, c);
-for(d = 0; d <= b; d++)
+q = drob(a, b, c);         //the first part of the formula
+for(d = 0; d <= b; d++)    //the second part of the formula
 q = q + factorial(d);
 
-printf("Q = %.6lf\n", q);
+printf("Q = %.6lf\n", q);  //print result with 6 numbers after point
 
 return 0;
 }
