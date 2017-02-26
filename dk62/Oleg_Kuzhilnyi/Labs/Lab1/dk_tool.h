7f88b42730ca3b314c37f03b5b22208970a457fe
Jayawardenepura <kuzhilnyioleg16@gmail.com>
2017-02-26 16:50:51 +0200

    Add files via upload

---
+++ dk62/Oleg_Kuzhilnyi/Labs/Lab1/dk_tool.h
@@ -0,0 +1,18 @@
+//
+//  dk_tool.h
+//  labs_KPI
+//
+//  Created by air on 23.02.17.
+//  Copyright © 2017 airMair. All rights reserved.
+//
+
+#ifndef dk_tool_h
+#define dk_tool_h
+
+#include <stdio.h>
+
+int MathST(int A);
+float MathDEL(int Number_A, int Number_B, int Number_C);
+int MathSUMM(int Number_A, int Number_B, int Number_C);
+
+#endif /* dk_tool_h */
