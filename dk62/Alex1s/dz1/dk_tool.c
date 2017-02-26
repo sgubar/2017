3c00fd22c3f71a7eb65092b96079def9dc88e52f
dest1nyd1spers1on <alexafa@ukr.net>
2017-02-26 18:27:40 +0200

    Add files via upload

---
+++ dk62/Alex1s/dz1/dk_tool.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+
+int enter_lines()
+{
+int lines = 0;
+do{
+	printf("Enter amount of lines: ");
+	scanf("%i", &lines);
+}while(lines<2);
+return lines;
+}
+
+
+//--------------------------------------------------------------------------------------------------------------------------------
+
+
+int enter_columns()
+{
+int columns = 0;
+do{
+	printf("Enter amount of columns: ");
+	scanf("%i", &columns);
+}while(columns<2);
+return columns;
+}
+
+
+//--------------------------------------------------------------------------------------------------------------------------------
+
+
+void write_matrix(int lines, int columns, float matrix[lines][columns])
+{
+int i = 0, j = 0;
+for(i = 0; i < lines; i++)
+for(j = 0; j < columns; j++) {
+printf("Enter variable[%i][%i]: ", i, j);   //[i][j] for better meaning position of elements
+scanf("%f", &matrix[i][j]);}
+}
+
+
+//--------------------------------------------------------------------------------------------------------------------------------
+
+
+void mnoj_matrix(float coef, int lines, int columns, float matrix[lines][columns])
+{
+printf("Enter coefficient of result matrix: ");
+scanf("%f", &coef);
+int i = 0, j = 0;
+for(i = 0; i < lines; i++)
+for(j = 0; j < columns; j++)
+matrix[i][j] *= coef;        //matrix[i][j] = matrix[i][j]*coef;
+}
+
+
+//--------------------------------------------------------------------------------------------------------------------------------
+
+
+void read_matrix(int lines, int columns, float matrix[lines][columns])
+{
+int i = 0, j = 0;
+for(i = 0; i < lines; i++){
+for(j = 0; j < columns; j++)
+printf("%.3f ", matrix[i][j]);
+printf("\n");}
+printf("\n");
+}
+
+
+//--------------------------------------------------------------------------------------------------------------------------------
+
+
---
+++ dk62/Alex1s/dz1/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dk_tool.h"
+
+int main(int argc, const char *argv[]) {
+
+int lines = 0, columns = 0;
+float coef = 0;
+lines = enter_lines();
+columns = enter_columns();
+float matrix[lines][columns];
+write_matrix(lines, columns, matrix);
+printf("\nYou wrote:\n");
+read_matrix(lines, columns, matrix);
+mnoj_matrix(coef, lines, columns, matrix);
+printf("\nResult matrix: \n");
+read_matrix(lines, columns, matrix);
+
+system("pause");
+return 0;
+}
+
