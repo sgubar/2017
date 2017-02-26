e6a2d22112ca88117131e6dc0b4fd518d74f4f94
dest1nyd1spers1on <alexafa@ukr.net>
2017-02-26 18:10:31 +0200

    Add files via upload

---
+++ dk62/Alex1s/laba1/dk_tool.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "dk_tool.h"
+
+unsigned long int factorial(int d) {
+if(d == 0)
+	{
+	int h = 1;
+	return h;
+	}
+
+int i = 0;
+unsigned long int result = 1;
+
+for(i = 1; i <= d; i++)
+    {
+    result = result * i;
+    }
+return result;
+}
+
+
+//--------------------------------------------------------------------------------------------------------------------------------
+
+
+double drob(int a, int b, int c)
+{
+if(b+c*c == 0)
+	{
+	printf("Error!\n");
+	return 1;
+	}
+int xx = b + (c*c);
+double qq = a*b;
+qq = qq/xx;
+return qq;
+}
+
