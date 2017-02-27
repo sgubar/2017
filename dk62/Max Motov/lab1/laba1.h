0b59fd5816384cdd780c225c594b469810f4ab2a
Madranko <motov.maxim@gmail.com>
2017-02-27 22:54:56 +0300

    Create laba1.h

---
+++ dk62/Max Motov/lab1/laba1.h
@@ -0,0 +1,10 @@
+#ifndef _laba1
+#define _laba1
+
+void Factorial(int Avalue, int *factorial);
+
+void Modul(int Avalue, int Bvalue, int *ModuledDenominator, int *denominator);
+
+void Result(int factorial, int *denominator, int *ModuledDenominator, float *result);
+
+#endif // _laba1
