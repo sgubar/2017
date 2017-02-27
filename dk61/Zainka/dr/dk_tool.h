48f75b2ef3803dca8f9deae7513fe48dd24f276c
Zainka37 <moran.a@list.ru>
2017-02-27 17:20:43 +0200

    Create dk_tool.h

---
+++ dk61/Zainka/dr/dk_tool.h
@@ -0,0 +1,16 @@
+
+
+#ifndef dk_tool_h
+#define dk_tool_h
+
+
+
+int matrix1(int a, b, A[a][b] ) 
+
+
+
+int matrix2(int b, c, B[b][c]);
+int vivod_matrix2( int b, c, B[b][c]);
+int result( C[i][j], B[a]b] , A[b][c]);
+int vivod_result( C[i][j], B[a]b] , A[b][c]);
+#endif
