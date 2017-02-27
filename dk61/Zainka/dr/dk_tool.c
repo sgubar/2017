4c2f8005619ceefb21467cb8fc044b00719e93d5
Zainka37 <moran.a@list.ru>
2017-02-27 17:21:12 +0200

    Create dk_tool.c

---
+++ dk61/Zainka/dr/dk_tool.c
@@ -0,0 +1,72 @@
+#ifndef <dk_tool.h>
+#define dk_tool.h
+
+int matrix1(int a, b, A[a][b] ) 
+{
+
+	for(i = 0 ; i<a ; i++)
+ {
+ 	for (j= 0 ; j < b ; j++)
+ 	{
+ 		scanf("%i", &A[i][j]);
+	}
+ }
+ {
+{
+ 
+ int vivod_matrix1(int a, b, A[a][b] ) 
+{
+ 
+ for(i = 0 ; i<a ; i++)
+ {
+ 	for (j= 0 ; j < b ; j++)
+ 	{
+ 		printf("%i ", A[i][j]);
+
+	}
+	printf("\n"); 
+}
+} 
+int matrix2(int b, c, B[b][c]); 
+ {
+ 
+ 	for(i = 0 ; i<b ; i++)
+ {
+ 	for (j= 0 ; j < c ; j++)
+ 	{
+printf("\n" , B[i][j]);
+}
+}
+}
+int vivod_matrix2( int b, c, B[b][c]);
+{
+for(i = 0; i < b; i++)
+{
+	for(j = 0; j <c; j++ )
+	{
+	printf("%i ", B[i][j]);
+	
+	}
+printf("\n"); 
+}
+}
+int result( C[i][j], B[a]b] , A[b][c]);
+{
+
+for(i = 0; i < a; i++ )
+for(j = 0; j < a; j++)
+{ 
+
+ for(k = 0; k < a ; k++)
+ C[i][j] += A[i][k] * B[k][j];
+}}
+int vivod_result( C[i][j], B[a]b] , A[b][c]);
+{
+
+printf("%i ", C[i][j]);
+}
+       printf("\n");
+}
+
+}
+#endif /* _DLL_H_ */
