1f47d152464d8bcadedabe1360a03eda58647c64
xxxrocoxxx <xperiaroco@gmail.com>
2017-02-27 23:07:02 +0200

    Lab1 added

---
+++ dk62/Dima_Belousov/Lab1/function.h
@@ -0,0 +1,37 @@
+#ifndef function_h
+#define function_h
+
+
+int modul(int a, int b,int c){
+
+int result = (a * c) - b;
+
+if(result < 0) return result * (-1);
+
+else return result;
+}
+
+int degree(int x){
+if(x == 0) return 1;
+
+else return x*x*x;
+}
+
+int sum(int x){
+
+int k;
+int  i,j;
+int result = 1;
+
+for(i = 0; i < x; i++){
+        k = 1;
+    for(j = 0; j < 1+i; j++){
+        k *= 2;
+    }
+    result += k;
+}
+
+return result;
+}
+
+#endif
