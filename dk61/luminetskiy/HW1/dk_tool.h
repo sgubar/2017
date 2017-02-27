262a5fee847ef075cd6f9c9678b36a0463cafb11
romamaka <lumineckiyy447@gmail.com>
2017-02-27 18:42:04 +0200

    Add files via upload

---
+++ dk61/luminetskiy/HW1/dk_tool.h
@@ -0,0 +1,13 @@
+//заголовчный файл
+#ifndef dk_tool_h
+#define dk_tool_h
+
+#include <stdio.h>
+
+
+
+
+void read_mat(int a,int b, int mat[a][b]);
+void print_mat(int a,int b, int mat[a][b]);
+void multiplicated_matrix(int a, int b, int mat[a][b], int v,int mat2[i][j]);
+#endif //dk_tool_h
