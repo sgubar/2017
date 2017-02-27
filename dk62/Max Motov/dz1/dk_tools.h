85beb3be8c4044ff70d397afba1faa23bdb57895
Madranko <motov.maxim@gmail.com>
2017-02-27 22:51:25 +0300

    Create dk_tools.h

---
+++ dk62/Max Motov/dz1/dk_tools.h
@@ -0,0 +1,9 @@
+#ifndef _dk_tool
+#define _dk_tool
+
+void EnterMatrixValues(int **matrix, int dimension);
+
+void MulMatrix(int **matrix1, int **matrix2, int **resultMatrix, int dimension);
+
+void MatrixOut(int **matrix, int dimension);
+#endif // _dk_tool
