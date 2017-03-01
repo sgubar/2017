485529b29bfc24a4ba11124dd66df571db7258a1
GARIIKK <ilya.moryachok@gmail.com>
2017-03-01 11:48:26 +0200

    Add files via upload

---
+++ dk61/GorunovIlya/HW1/dk_tool.h
@@ -0,0 +1,18 @@
+#ifndef dk_tool_h
+#define dk_tool_h
+void debugEnterSentence(int maxLenSentence , char sentence[] );
+
+void debugEnterWord(int maxLenWord , char word[]);
+
+int search( char  firstWord[] , char  secondWord[]);
+
+   
+
+
+   
+
+
+
+
+
+#endif /* dk_tool_h */
