1e6b21b44f4e2dd272cadddea4587ba32e165541
JamsAurom <maksimaldokhin84@gmail.com>
2017-03-05 20:58:53 +0200

    Add files via upload

--- dk61/Aldokhin/LW2/main.c
+++ dk61/Aldokhin/LW2/main.c
@@ -20,10 +20,17 @@ int main() {
     //array's size
     int msize=scan_size();
     array_main=malloc(msize* sizeof(int));
-    array_sort=malloc(msize* sizeof(int));
 
-    if (NULL==array_main || NULL==array_sort)
+    if (NULL==array_main)
     {
+        printf("Error, memory overflow.");
+        return 0;
+    }
+
+    array_sort=malloc(msize* sizeof(int));
+    if (NULL==array_sort)
+    {
+        free(array_main);
         printf("Error, memory overflow.");
         return 0;
     }
@@ -72,48 +79,8 @@ int main() {
     }
 
 
-//    //The record is not sorted array
-//    Write_in_file(f_save, 2, msize, array_main);
-//
-//    //entry array method bubble
-//    overstore_array(array_main, array_sort, msize); //copy the array to sort array
-//    time=clock();
-//    sort_bubble(array_sort, msize);
-//    time=clock()-time;
-//    Write_time(f_save, 3, time);
-//    Write_in_file(f_save, 3, msize, array_sort);
-//
-//    //entry array method selection
-//    overstore_array(array_main, array_sort, msize); //copy the array to sort array
-//    time=clock();
-//    sort_selection(array_sort, msize);
-//    time=clock()-time;
-//    Write_time(f_save, 4, time);
-//    Write_in_file(f_save, 4, msize, array_sort);
-//
-//    //entry array method insert
-//    overstore_array(array_main, array_sort, msize); //copy the array to sort array
-//    time=clock();
-//    sort_insert(array_sort, msize);
-//    time=clock()-time;
-//    Write_time(f_save, 5, time);
-//    Write_in_file(f_save, 5, msize, array_sort);
-//
-//    //entry array method Shell
-//    overstore_array(array_main, array_sort, msize); //copy the array to sort array
-//    time=clock();
-//    sort_Shell(array_sort, msize);
-//    time=clock()-time;
-//    Write_time(f_save, 6, time);
-//    Write_in_file(f_save, 6, msize, array_sort);
-//
-//    //entry array method Quick Sort
-//    overstore_array(array_main, array_sort, msize); //copy the array to sort array
-//    time=clock();
-//    sort_QuickSort(array_sort, 0, msize);
-//    time=clock()-time;
-//    Write_time(f_save, 7, time);
-//    Write_in_file(f_save, 7, msize, array_sort);
+    free(array_main);
+    free(array_sort);
 
     make_futter_file(f_save);
     closefile(f_save);
