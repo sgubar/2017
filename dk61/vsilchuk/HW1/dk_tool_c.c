321279917baa307eb61f9c57e65fb01bdb860278
Vitaliy Silchuk <nubb99@gmail.com>
2017-03-06 22:53:10 +0100

    Add files via upload

---
+++ dk61/vsilchuk/HW1/dk_tool_c.c
@@ -0,0 +1,35 @@
+#include "dk_tool.h"
+#include <stdio.h>
+#include <string.h>
+
+int Search(char TheString[], char TheWord[])
+{
+	int PositionW = 0;
+	int PositionStr = 0;
+	int Coordinates = -1;
+	int Counter = 0;
+	
+	while(PositionStr < strlen(TheString))
+	{																	
+			for(int n = 0; n < strlen(TheWord); n++)
+			{																			
+				if(TheWord[PositionW+n] == TheString[PositionStr+n])
+				{
+				Counter++;																//счётчик совпадений символов
+					if(Counter == strlen(TheWord))
+					{
+						Coordinates = PositionStr+1;
+						return Coordinates;										
+					}					
+				}				
+			}	
+			Counter = 0;
+			PositionStr++;
+	}
+	
+	if(Coordinates = -1)																//если совпадений нет
+ 	{
+ 		return -1;
+	}
+}
+
