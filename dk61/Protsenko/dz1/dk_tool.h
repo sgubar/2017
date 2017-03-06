0c98924164901313529e388f9dff894b56e6a7b8
ilyaprotsenko <ilyaprotsenko@ukr.net>
2017-03-06 21:48:44 +0200

    Create dk_tool.h

---
+++ dk61/Protsenko/dz1/dk_tool.h
@@ -0,0 +1,13 @@
+//  dk_tool.h
+//  dz1
+//  Created by Illia Protsenko on 3/1/17.
+#ifndef dk_tool_h
+#define dk_tool_h
+
+#include <stdio.h>
+
+void filling_matrix(int x, int y, int arr[x][y], int arr2[x][y]);
+void display_matrix(int x, int y, int arr[x][y], int arr2[x][y]);
+void matrix_diff(int x, int y, int arr[x][y], int arr2[x][y], int diff[x][y]);
+
+#endif /* dk_tool_h */
