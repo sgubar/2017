6a657e28e8b1462b578b1162aa6f5d18be4ef6e3
MaksGolub <maks.golub.1991@gmail.com>
2017-03-07 13:41:18 +0200

    Update main.c

--- dk62/MaksGolub/lab_rab1/main.c
+++ dk62/MaksGolub/lab_rab1/main.c
@@ -6,17 +6,17 @@ int main(void)
  double A=0, B=0, C=0;
  double drob=0, Q=0;
  printf("Enter the constant A:");
- scanf("%lf", &A);
+ scanf("%lf", &A);             //зчитування констант
  printf("Enter the constant B:");
  scanf("%lf", &B);
  printf("Enter the constant C:");
  scanf("%lf", &C);
- drob=(A+B+C)/(100-B-C);
- if(100-B-C!=0)
+ drob=(A+B+C)/(100-B-C); //дробова дія
+ if(100-B-C!=0) //перевірка на нуль в знаменнику
  {
  double X = func_pow(A);
  Q=X+drob;
- printf("Result:%f\n", Q);
+ printf("Result:%f\n", Q); //вивід
 }
 else
 {
