fed5bbaf165cf8276625b58aff4db03022782d60
MaksGolub <maks.golub.1991@gmail.com>
2017-03-07 13:39:18 +0200

    Update dk_tool.c

--- dk62/MaksGolub/dk_tool.c
+++ dk62/MaksGolub/dk_tool.c
@@ -3,14 +3,14 @@
 
 int func_sub_print(int h, int w, int matr1[h][w], int matr2[h][w])
 {
- int result[h][w]; \\декларація результативної матриці
+ int result[h][w]; //декларація результативної матриці
  int i=0, j=0;
- while (i<h)    \\за допомогою циклу здійснюється віднімання 
+ while (i<h)    //за допомогою циклу здійснюється віднімання 
   {
    while(j<w)
     {
      result[i][j]=matr1[i][j]-matr2[i][j];
-     printf("%d ", result[i][j]); \\вивід матриці 
+     printf("%d ", result[i][j]); //вивід матриці 
      j++;
     } 
     j=0;
