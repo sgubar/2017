46eb175f239248315d228a2fe87c43625f8296ab
MaksGolub <maks.golub.1991@gmail.com>
2017-03-07 12:31:29 +0200

    Update main.c

--- dk62/MaksGolub/lab_rab1/main.c
+++ dk62/MaksGolub/lab_rab1/main.c
@@ -5,17 +5,22 @@ int main(void)
 {
  double A=0, B=0, C=0;
  double drob=0, Q=0;
- \\опитування значень
  printf("Enter the constant A:");
- scanf("%lf", &A); 
+ scanf("%lf", &A);
  printf("Enter the constant B:");
  scanf("%lf", &B);
  printf("Enter the constant C:");
  scanf("%lf", &C);
  drob=(A+B+C)/(100-B-C);
- \\виклик функції
+ if(100-B-C!=0)
+ {
  double X = func_pow(A);
  Q=X+drob;
  printf("Result:%f\n", Q);
+}
+else
+{
+	printf("Error\n");
+}
 }
  
