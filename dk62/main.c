0cffb88b7bc337625769d692bc3e7175a1d12709
I-DefenDer <mhuhbjp@yandex.ua>
2017-03-14 00:25:24 +0200

    Add files via upload

---
+++ dk62/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int denominator = 0; //знаменатель
+    int value_A = 0;
+    int value_B = 0;
+    int faktorial = 1;
+    float result = 0;
+
+    do
+    {
+        printf("Enter A > 0:\n");
+        wrong_A:
+        scanf("%i" , &value_A);
+        if( value_A <= 0)
+        {
+            printf("Wrong value A!\n");
+        }
+    }
+    while (value_A <= 0);
+     if( value_A <= 0)
+        {
+            printf("Wrong value A!\n Please enter A again:\n");
+            goto wrong_A;
+        }
+
+    Faktorial(value_A , &faktorial); //функція знаходження факторіалу числа
+
+    Modul(value_A , value_B , &denominator); //функція знаходження модулю числа
+
+    Result(faktorial , denominator , &result);
+
+    getchar();
+    return 0;
+
+}
