32ab1b8c83a140d82a1bd41badb192436ab0a53d
Stanis209 <dante123321123@gmail.com>
2017-03-13 23:59:33 +0200

    Create dk_tool.h

---
+++ dk61/Havulyak/Dz1/dk_tool.h
@@ -0,0 +1,12 @@
+//  dk_tool.h
+//  dz1
+//  Created by Stanislav Havulyak on 13/3/17.
+
+#ifndef dk_tool_h
+#define dk_tool_h
+
+#include <stdio.h>
+
+void create_matrix(int x, int y, int matr1[x][y], int matr2[x][y], int matr_otvet[x][y]);
+
+#endif 
