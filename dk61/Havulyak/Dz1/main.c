173aa069180c45316f6465de873446928037d673
Stanis209 <dante123321123@gmail.com>
2017-03-13 23:58:21 +0200

    Update main.c

--- dk61/Havulyak/Dz1/main.c
+++ dk61/Havulyak/Dz1/main.c
@@ -1,1 +1,15 @@
+//  main.c
+//  dz1 
+//  Created by Stanislav Havulyak on 13/3/17.
+
+
+#include <stdio.h>
+#include "dk_tool.h"
+
+int main(void)
+{
+	int x, y, matr1[x][y], matr2[x][y], matr_otvet[x][y];
+    create_matrix(x, y, matr1, matr2, matr_otvet);
+}
+
 
