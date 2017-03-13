659ec31c424a29a2815e54f47ac42b5fb6571183
I-DefenDer <mhuhbjp@yandex.ua>
2017-03-14 00:33:08 +0200

    Delete Lab_Rab1.h

--- dk62/Lab_Rab1.h
+++
@@ -1,6 +0,0 @@
-#ifndef _Lab_Rab1
-#define _Lab_Rab1
-void Faktorial(int value_A , int *faktorial);
-void Modul(int value_A , int value_B , int *denominator);
-void Result(int factorial , int denominator , float *result);
-#endif // Lab_Rab1
