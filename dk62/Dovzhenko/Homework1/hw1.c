c67182fd22c36f482f2f140f65f211ec5be58532
DovzhenkoD <d.dovgenko@gmail.com>
2017-03-14 00:56:22 +0200

    Update hw1.c

--- dk62/Dovzhenko/Homework1/hw1.c
+++ dk62/Dovzhenko/Homework1/hw1.c
@@ -1,1 +1,25 @@
+#include "hw.h"
+#include <stdio.h>
+#include <string.h>
+ int SearchWord(char Sentence[], char Word[])
+ {
+ 	int counter = 0;
+ 	for (int k =0; k<strlen(Sentence); k++)
+ 	{
+ 		if(Sentence[k]==Word[0])
+ 		{
+ 			for(int t=0; t<strlen(Word); t++)
+ 			{
+ 				if(Sentence[k+t]==Word[t])
+ 				counter++;
+ 				if (counter==strlen(Word))
+ 				return (k);
+			}
+		}
+		counter=0;
+	}
+ 	
+ if (counter == 0)
+ return -1;	
 
+ }
