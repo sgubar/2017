5f0ae14e8a91885e3f7fa608b4ec96439fa79247
SKudlai <slava-16@ukr.net>
2017-03-15 00:01:39 +0200

    Update main.c

--- dk61/Kudlais_folder/Homework_0/dz1/main.c
+++ dk61/Kudlais_folder/Homework_0/dz1/main.c
@@ -3,14 +3,14 @@
 #include "dk_tools.h"
 
 int main(void){
-    int results [5];
+    int results [5]; // 
     string s;
     bool acces = false;
     while(acces == false ){
          printf("Take please 8 or more numbers which have 4 or less symbols without typing any letter:\n");
          s = GetString();
          int length = strlen(s);
-         acces = check();
+         acces = check(s);
     }
     for(int k = 0; k < 5; k++){
         printf("%i\n",results[k]);
