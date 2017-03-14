b577962b19081fe380a5ebb7b1a118b4e5960891
hogl030 <hogl030@gmail.com>
2017-03-14 23:56:01 +0200

    Create dk_tool.h

---
+++ dk61/DolgopolovV/Dz1/dk_tool.h
@@ -0,0 +1,10 @@
+#ifndef dk_tool.h
+#define dk_tool.h
+
+#include <stdio.h>
+
+void reading(int size, int mas[size]);
+void swap(int size, int mas[size], int obmen);
+void print_m(int size, int mas[size]);
+
+#endif
