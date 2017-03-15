a869f952cb08b7e37ac5f2263bc84cb12692d58a
SKudlai <slava-16@ukr.net>
2017-03-16 00:06:22 +0200

    Update main.c

--- dk61/Kudlais_folder/Homework_0/dz1/main.c
+++ dk61/Kudlais_folder/Homework_0/dz1/main.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
-#include <cs50.h>
+#include <stdbool.h>
+#include <string.h>
 #include "dk_tools.h"
 
 int main(void){
     int results [5]; // 5 використовується як приблизна кількість елеметнів, які потрібно отримати
-    string s;
+    string str;
     bool acces = false;
     while(acces == false ){ //цикл не дає можливоті користувачеві вийти з нього, поки він не введе правильні значення
          printf("Take please 8 or more numbers which have 4 or less symbols without typing any letter:\n");
-         s = GetString();
-         int length = strlen(s);
-         acces = check(s);
+         scanf("%s",&str);
+         int length = strlen(str);
+         acces = check(str);
     }
     for(int k = 0; k < 5; k++){
         printf("%i\n",results[k]);
