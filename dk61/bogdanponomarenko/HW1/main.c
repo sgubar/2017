d9242e3d7597d515bb7030ecca9deadb4a32588e
Bogdan Ponomarenko <pb6798@gmail.com>
2017-03-20 22:05:00 +0200

    Update main.c

--- dk61/bogdanponomarenko/HW1/main.c
+++ dk61/bogdanponomarenko/HW1/main.c
@@ -2,24 +2,31 @@
 #include "dk_tool.h"
 int main(int argc, char *argv[])
 {
-int row;
-int col;
-int i;
-int j;
-static int first_matrix[10][10];
-static int second_matrix[20][20];
+    int row;
+    int col;
+    int i;
+    int j;
+    static int first_matrix[10][10];
+    static int second_matrix[20][20];
 
-printf("Dimension of matrix:\n");
-scanf("%i %i", &row, &col);
+    printf("Dimension of matrix:\n");
+    scanf("%i %i", &row, &col);
 
-if (row < 0 || col <= 0) {
-printf("Error\n");
-}
+    if (row < 0 || col <= 0)
+    {
+        printf("Error\n");
+    }
 
-display_first_matrix(row, col, first_matrix);
 
-display_second_matrix(row, col, second_matrix);
+    printf("Elements of first_matrix:\n");
+    display_first_matrix(row, col, first_matrix);
 
-sum(row, col, first_matrix, second_matrix);
+
+    printf("Elements of second_matrix:\n");
+    display_second_matrix(row, col, second_matrix);
+
+
+    printf("Sum of first_matrix and second_matrix:\n");
+    sum(row, col, first_matrix, second_matrix);
 
 }
