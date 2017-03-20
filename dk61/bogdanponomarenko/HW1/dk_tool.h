4054c72a871bedc3f3aa07a37cb0823a12f85ad5
Bogdan Ponomarenko <pb6798@gmail.com>
2017-03-20 21:30:29 +0200

    Create dk_tool.h

---
+++ dk61/bogdanponomarenko/HW1/dk_tool.h
@@ -0,0 +1,10 @@
+#ifndef dk_tool_h
+#define dk_tool_h
+
+#include <stdio.h>
+
+void display_first_matrix(int row, int col, int first_matrix[10][10]);
+void display_second_matrix(int row, int col,  int second_matrix[20][20]);
+void sum(int row, int col,  int first_matrix[10][10], int second_matrix[20][20]);
+
+#endif
