4a692b2df47820bcda8b7851fe1bc9fbd8faf92b
Bogdan Ponomarenko <pb6798@gmail.com>
2017-03-21 02:23:28 +0200

    Update main.c

--- dk61/bogdanponomarenko/HW1/main.c
+++ dk61/bogdanponomarenko/HW1/main.c
@@ -1,32 +1,32 @@
 #include <stdio.h>
-#include "dk_tool.h"
+#include "dk_tool.h" /*підключення директиви, з яких препроцесор зчитує зміст файлу, що знаходиться в одній папці з виконавчим файлом, а також виконує директиви і підстановки*/
 int main(int argc, char *argv[])
 {
-    int row;
-    int col;
-    int i;
-    int j;
-    static int first_matrix[10][10];
-    static int second_matrix[20][20];
+    int row; //ініціалізація змнних
+    int col; //ініціалізація змнних
+    int i; //ініціалізація змнних
+    int j; //ініціалізація змнних
+    static int first_matrix[10][10]; //ініціалізація змнних
+    static int second_matrix[20][20]; //ініціалізація змнних
 
     printf("Dimension of matrix:\n");
-    scanf("%i %i", &row, &col);
+    scanf("%i %i", &row, &col); //зчитування значень довжини та висоти рядка так сповпця матриці, введених користувачем з клавіатури
 
-    if (row < 0 || col <= 0)
+    if (row < 0 || col <= 0) // перевірка введених даних на додатність
     {
-        printf("Error\n");
+        printf("Error\n");//виведення повідомлення про помилку у разі від'ємності принаймні одного зі значень
     }
 
 
     printf("Elements of first_matrix:\n");
-    display_first_matrix(row, col, first_matrix);
+    display_first_matrix(row, col, first_matrix); //виведення на екран уже готової 1 матриці. Код для опитування значеннь матриці розміщений у файлі "dk_tool.h" для кращого зовнішньго вигляду основної програми
 
 
     printf("Elements of second_matrix:\n");
-    display_second_matrix(row, col, second_matrix);
+    display_second_matrix(row, col, second_matrix); //виведення на екран уже готової 2 матриці. Код для опитування значеннь матриці розміщений у файлі "dk_tool.h" для кращого зовнішньго вигляду основної програми
 
 
     printf("Sum of first_matrix and second_matrix:\n");
-    sum(row, col, first_matrix, second_matrix);
+    sum(row, col, first_matrix, second_matrix); //виведення на екран суми 2 матриць
 
 }
