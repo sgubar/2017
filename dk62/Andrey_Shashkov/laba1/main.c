42806003a961ccc6759caa41207fd46023ccbe2f
AShashkov <andr-sh@ukr.net>
2017-03-22 08:42:22 +0200

    Update main.c

--- dk62/Andrey_Shashkov/laba1/main.c
+++ dk62/Andrey_Shashkov/laba1/main.c
@@ -1,24 +1,16 @@
 #include <stdio.h>
-#include <string.h>
-#include <ctype.h>
-#include <math.h>
 #include "dk_tool.h"
 
 int main(int argc, char *argv[]) 
 
 {
-	int b = 0;
-	int k = 0;
+	int F = 0;
+	int result = 0;
 	int D = 0;
 	int C = 0;
-	printf ( "Plese, enter First nomber: \n");
-    scanf("%d", &D) ;
-	printf ( "Plese, enter Second nomber: \n");
-    scanf("%d", &C) ;
-    printf ( "Plese, enter nomber on  16 poinet: \n");
-    scanf("%x", &b) ;
-    
-  k = AQUATION(D, C ,b);
-  printf("Result: %d",k);
+	printf("enter First nomber,enter Second nomber and enter nomber on hex sistem \n");
+	scanf("%d %d %x", &D,&C,&F) ;
+    result = AQUATION(D , C , F);
+    printf("Result: %d",result);
 	return 0;
 }
