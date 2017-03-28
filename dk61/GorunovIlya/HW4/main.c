309ebb7be69efbe9362f723e53eef32d7b7d871f
GARIIKK <ilya.moryachok@gmail.com>
2017-03-28 18:16:11 +0300

    Add files via upload

---
+++ dk61/GorunovIlya/HW4/main.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include "dk_tool.h"
+#include <string.h>
+
+int main()
+{
+	SphereList * theSphere = createSphereList(8);
+	FILE * theFile = fopen("List.json","w");
+	
+	addSphere(theSphere, 2, 2, 2, 8);
+	addSphere(theSphere, 3, 3, 3, 4);
+	addSphere(theSphere, 4, 4, 4, 6);
+	addSphere(theSphere, 5, 5, 5, 2);
+	addSphere(theSphere, 6, 6, 6, 9);
+	addSphere(theSphere, 7, 7, 7, 7);
+	
+	printf("Enter X coordinate: ");
+	int x = enterParametersOfSphere();
+	printf("Enter Y coordinate: ");
+	int y = enterParametersOfSphere();
+	printf("Enter Z coordinate: ");
+	int z = enterParametersOfSphere();
+	printf("Enter Radius: ");
+	int radius = enterParametersOfSphere();
+	
+	addSphere(theSphere, x, y, z, radius);
+	
+	writeInFile(theFile,theSphere);
+	fprintf(theFile,"\n----------------After sorting------------------------\n");
+	
+	
+	SpherePosition firstSphere = theSphere ->spheres[6];
+	printf("X, Y, Z   -  %i, %i, %i and radius is: %i;\n",firstSphere.xPosition ,firstSphere.yPosition ,firstSphere.zPosition,firstSphere.radius);
+	
+	float area = areaCalculation(&firstSphere);
+    printf("The area of sphere is: %.3f;\n", area);
+
+
+    printf("\nBefore sorting...\n");
+	writeListOnScreen(theSphere);
+	printf("----------------------------------------------\n");
+	//bubbleSort(theSphere);
+	//selectionSort(theSphere);
+	//insertionSort(theSphere);
+	
+	//bubbleSort2(theSphere);
+	//selectionSort2(theSphere);
+	insertionSort2(theSphere);
+	
+	printf("After sorting....\n");
+	writeListOnScreen(theSphere);
+	
+	writeInFile(theFile,theSphere);
+	
+	
+	destroySphereList(theSphere);
+	
+	fflush(theFile);
+	fclose(theFile);
+	return 1;
+	
+}
+
---
+++ dk61/GorunovIlya/HW4/sort.h
@@ -0,0 +1,15 @@
+#ifndef sort_h
+#define sort_h
+#include "dk_tool.h"
+void bubbleSort(SphereList *aSphere);
+void selectionSort(SphereList *aSphere);
+void insertionSort(SphereList *aSphere);
+
+
+void bubbleSort2(SphereList *aSphere);
+void selectionSort2(SphereList *aSphere);
+void insertionSort2(SphereList *aSphere);
+
+
+
+#endif
