babcac2b92b43deb0548126bb8c99811cd78451f
Zainka37 <moran.a@list.ru>
2017-03-29 09:16:06 +0300

    Update main.c

--- dk61/Zainka/lr1/main.c
+++ dk61/Zainka/lr1/main.c
@@ -6,44 +6,41 @@
 
 int main()
 {
-int B;
-int D;
-int G;
-int A;
-int K;
-int Otvet;
-int C;
- int R;
+float B;
+float D;
+float G;
+float A;
+float K;
+float Otvet;
+float C;
+ float R;
 
 printf("Vvedit tchislo A\n");
-scanf("%i" , & A);
+scanf("%f" , & A);
 {
 G = pow2(A); 
-printf(" %i stepen" , G);
+printf(" %f stepen\n" , G);
 }
 
 printf("Vvedit tchislo B\n");
-scanf("%i" , & B);
+scanf("%f" , & B);
 {
 
 D = pow2(B) ;	
-printf(" %i" , D);
+printf(" %f\n" , D);
 }
 {
 
 K = fact(B);
-printf(" %i" , K);
+printf(" %ffact\n" , K);
 }
 printf("vvod tchislo C\n");
-scanf("%i" , & C);
+scanf("%f" , & C);
 R = pow2(C);
-printf(" %i" , R);
+printf(" %f\n" , R);
 
 Otvet = (G + D)/(B + R) + K;
-printf(" %i = otvet" , Otvet);
-
-
-
+printf(" %f = otvet" , Otvet);
 }
 
 
