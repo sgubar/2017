f78f6dcfc6ba7875de8895c2b41d4573c1db9eda
Bogdan Ponomarenko <pb6798@gmail.com>
2017-03-29 08:52:12 +0300

    Update dk_tool.h

--- dk61/bogdanponomarenko/HW1/dk_tool.h
+++ dk61/bogdanponomarenko/HW1/dk_tool.h
@@ -3,8 +3,8 @@
 
 #include <stdio.h>
 
-void display_first_matrix(int row, int col, int first_matrix[10][10]); //виведення 1 матриці на екран
-void display_second_matrix(int row, int col,  int second_matrix[20][20]); // виведення 2 матриці на екран
-void sum(int row, int col,  int first_matrix[10][10], int second_matrix[20][20]); //виведення суми 2 матриць на екран
+void display_first_matrix(int row, int col, int first_matrix[10][10]); //ініціалізація функції виведення 1 матриці на екран
+void display_second_matrix(int row, int col,  int second_matrix[20][20]); //ініціалізація функції виведення 2 матриці на екран
+void sum(int row, int col,  int first_matrix[10][10], int second_matrix[20][20]); //ініціалізація функції виведення суми 2 матриць на екран
 
 #endif
