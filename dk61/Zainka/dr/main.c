78ec5f822af14155cadd2638c463a11cee09cddf
Zainka37 <moran.a@list.ru>
2017-03-29 08:44:45 +0300

    Update main.c

--- dk61/Zainka/dr/main.c
+++ dk61/Zainka/dr/main.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
- #include <conio.h>                                            
+#include <conio.h>                                            
 #include <ctype.h>
-
-
-int main( )
+#include "dk_tool.h"
+#define N 4
+#include <time.h>
+int main()
 {
- 
+
 int a = 2; 
 int b = 2;
 int c = 2;
 int i = 2;
 int j = 2;
 int k = 0;
-int  C[i][j] = 0;
 int C[i][j];
- int A[a][b];
- int B[b][c];
- 	
+int A[a][b];
+int B[b][c];
 printf("Give me %i numbers 1 matrix, please:\n", a*b); 
-matrix1( a, b, A);
-vivod_matrix1(a ,b ,A);
-
-   printf("Give  me %i numbers 2 matrix, please:\n", a*c); 
- matrix2( b, c, B); 	
+matrix1( a, b , A);
+vivod_matrix2(a , b , A);
+printf("Give  me %i numbers 2 matrix, please:\n", a*c); 
+matrix2( b, c, B); 	
 vivod_matrix2(b , c , B);
-result(C , B, A);
-vivod_result(C , B, A);
+result(i , j , a, b, c, A , B ,C);
+return 0;
+}
+
 
 
 
