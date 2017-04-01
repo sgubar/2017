1c24faeb27348100a18c3d59c62c0443d68beef8
dest1nyd1spers1on <alexafa@ukr.net>
2017-04-01 16:34:55 +0300

    Update main.c

--- dk62/Alex1s/dz1/main.c
+++ dk62/Alex1s/dz1/main.c
@@ -6,8 +6,10 @@ int main(int argc, const char *argv[]) {
 
 int lines = 0, columns = 0;
 float coef = 0;
-lines = enter_lines();
-columns = enter_columns();
+printf("Enter amount of lines: ");
+lines = enter_value();
+printf("Enter amount of columns: ");
+columns = enter_value();
 float matrix[lines][columns];
 write_matrix(lines, columns, matrix);
 printf("\nYou wrote:\n");
