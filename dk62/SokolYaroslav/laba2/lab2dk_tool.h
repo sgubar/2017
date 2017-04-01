7f1f61f2f3d9d1c77f0f96eb3d1e38edf93849d5
Llokos <Llokos@meta.ua>
2017-04-01 18:49:21 +0300

    Create lab2dk_tool.h

---
+++ dk62/SokolYaroslav/laba2/lab2dk_tool.h
@@ -0,0 +1,12 @@
+#ifndef lab2dk_tool_h
+#define lab2dk_tool_h
+
+void generate (int kolslov,int kolelement,char slovoelement [kolslov][kolelement]);
+int inputint(int input, int n);
+void pechat(int kolslov,int kolelement,char slovoelement [kolslov][kolelement]);
+void zmeshchenie (int kolslov,int kolelement,char slovoelement [kolslov][kolelement],int i);
+void buble(int kolslov,int kolelement,char slovoelement [kolslov][kolelement]);
+
+
+
+#endif
