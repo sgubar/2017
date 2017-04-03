0384043289828e872e13f3df58979b0b02e7fff0
maksimo0 <mkdjsl@gmail.com>
2017-04-03 22:30:13 +0300

    Create dk_tool.h

---
+++ dk62/maksimo0/HW2/dk_tool.h
@@ -0,0 +1,41 @@
+//
+// Created by Max on 28.03.2017.
+//
+
+#ifndef dk_tool_h
+#define dk_tool_h
+#include <stdio.h>
+#include <string.h>
+
+typedef struct Koordinaty
+{
+    int Koordinata_X;
+    int Koordinata_Y;
+}Tochka;
+
+typedef struct Tochki
+{
+    Tochka *Tochka_A;
+    Tochka *Tochka_B;
+    Tochka *Tochka_C;
+    int Ploscha;
+}Trykutnyk;
+
+typedef struct TagTrK
+{
+    Trykutnyk *TrK;
+    int CurrentTrK;
+    int MaxKilkTrK;
+}SpskOfTrK;
+
+SpskOfTrK *StvorennyaSpsK(int RzmR);
+
+int addDani(SpskOfTrK *TagSpskOfTrK, int X1, int Y1, int X2, int Y2, int X3, int Y3);
+
+void Vidalennya(SpskOfTrK *TagSpskOfTrK);
+
+int ObchislennyaPloschi(Trykutnyk *TrK);
+
+void Print(SpskOfTrK *TagSpskOfTrK);
+
+#endif
