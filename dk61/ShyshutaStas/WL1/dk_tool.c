af60599a0dfdd7ba68f6e0e73b7ab6f47f459b0e
ShyshutaStas <3p141592stas@gmail.com>
2017-04-04 10:59:23 +0300

    Update dk_tool.c

--- dk61/ShyshutaStas/WL1/dk_tool.c
+++ dk61/ShyshutaStas/WL1/dk_tool.c
@@ -2,11 +2,11 @@
 int sum_series(int D,int C,int F)
 {   
     int result = 0;
-    int A = 1;
+    int A = 1;	
     while(A <= D)
 	{
 	result = A * (F - C) + result;
 	A++;
-    } 
+	} 
     return result; 
 }
