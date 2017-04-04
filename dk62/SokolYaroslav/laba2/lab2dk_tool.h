12352bb1366d2ca85c2cccabb47488ac9fdd973c
Llokos <Llokos@meta.ua>
2017-04-05 00:13:35 +0300

    Update lab2dk_tool.h

--- dk62/SokolYaroslav/laba2/lab2dk_tool.h
+++ dk62/SokolYaroslav/laba2/lab2dk_tool.h
@@ -1,12 +1,13 @@
 #ifndef lab2dk_tool_h
 #define lab2dk_tool_h
-
-void generate (int kolslov,int kolelement,char slovoelement [kolslov][kolelement]);
-int inputint(int input, int n);
-void pechat(int kolslov,int kolelement,char slovoelement [kolslov][kolelement]);
-void zmeshchenie (int kolslov,int kolelement,char slovoelement [kolslov][kolelement],int i);
-void buble(int kolslov,int kolelement,char slovoelement [kolslov][kolelement]);
-
-
-
+int inputint(int input, int limit);//введення та перевірка числа
+void generatestring (int kolslov,int kolelement,char slovoelement [kolslov][kolelement]);//генерація слів
+void print(int kolslov,int kolelement,char slovoelement [kolslov][kolelement]);//друкує згенерований текст
+void printfile(int kolslov,int kolelement,char filestring [kolslov][kolelement]);//друкує текс з файлу
+void printinf(int kolslov,int kolelement,char filestring [kolslov][kolelement]);//друкує текст в файл
+int razmerf(int kolelement);//дізнає кількість елементів в файлі(в рядку)
+int kolslovf(int kolelement,int kolslov);//дізнає кількість слів в файлі(в рядку)
+void obmenslov (int kolslov,int kolelement,char slovoelement [kolslov][kolelement],int i);//обмін розташувань двох сусідніх слів
+void bublesort(int kolslov,int kolelement,char slovoelement [kolslov][kolelement]);//сортування бульбашкою
+void filestingr(int kolelement,int kolslov,char filestring [kolslov][kolelement]);//зчитування масиву з файлу
 #endif
