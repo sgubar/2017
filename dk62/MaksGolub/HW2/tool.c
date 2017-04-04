855cc77c654d0027d02c9460271779099a91cbc6
MaksGolub <maks.golub.1991@gmail.com>
2017-04-04 12:51:09 +0300

    Update tool.c

--- dk62/MaksGolub/HW2/tool.c
+++ dk62/MaksGolub/HW2/tool.c
@@ -7,13 +7,13 @@
  
 FigureList *createFigureList(int aSize) //create List of Figure;
 {
-    FigureList *List = (FigureList *)malloc(sizeof(FigureList)); //create List and memory;
+    FigureList *List = (FigureList *)malloc(sizeof(FigureList));  //create List and memory;
     if(List != NULL)
 	{
 		List->paralel = (Figure *)malloc(aSize * sizeof(Figure)); //create memory for figure;
 		memset(List->paralel, 0, sizeof(Figure));
-		List->size = aSize; //quantity of figure;
-		List->current_size = 0;	//flag of figure;	
+		List->size = aSize;          //quantity of figure;
+		List->current_size = 0;		//flag of figure;
 	}
 	return List;
 }
@@ -26,12 +26,12 @@ int addCoor(FigureList *aFigure) //adding coordinate of points;
 		{
 			Figure *theFigure = &(aFigure->paralel[aFigure->current_size]);
 			if( NULL != theFigure)
-			{ int num, i=0;
+			{ int i=0;
 			  theFigure->points = (Point *)malloc(sizeof(Point)*8); //create memory for points;
 				for(i=0; i<8; i++)
 				{
 				printf("Enter the point[%d]\n", i+1);
-				theFigure->points[i].x = scan_f(); 
+				theFigure->points[i].x = scan_f();
 				theFigure->points[i].y = scan_f();
 			    theFigure->points[i].z = scan_f();
 			    }
@@ -69,13 +69,13 @@ int printFigure(FigureList *aFigure)
 
      Figure *theFigure = &(aFigure->paralel[i]); 
      	
-	printf("Figure [%d]\n", i + 1);
-	printf("the square of figure is %d\n", abs(square(theFigure))); //print square figure;
+	printf("Figure [%d]\n", i + 1); 
+	printf("the square of that figure is %d\n\n", abs(square(theFigure))); //print square figure;
 
      for (j = 0; j < 8; j++) 
       { 
 
-      printf("point[%d]={%d, %d, %d}\n", j+1, theFigure->points[j].x, theFigure->points[j].y, theFigure->points[j].z); //print point of figure;
+      printf("point[%d]=[%d, %d, %d]\n\n", j+1, theFigure->points[j].x, theFigure->points[j].y, theFigure->points[j].z); //print point of figure;
 
       }    
 
@@ -94,12 +94,10 @@ int scan_f(void)
     
 int square(Figure *Figure)
 {
+
 			double L12 = 0;
 			double L23 = 0;
-			double L15 = 0;
 			double L25 = 0;
-			double L27 = 0;
-			double L13 = 0;
 			double S1,S2,S3,square;
 			double x=0, y=0, z=0;
 			square = 0;
@@ -107,14 +105,14 @@ int square(Figure *Figure)
 			x = (Figure->points[1].x) - (Figure->points[2].x);
 			y = (Figure->points[1].y) - (Figure->points[2].y);
 			z = (Figure->points[1].z) - (Figure->points[2].z);
-			L12 = sqrt((x*x) + (y*y) + (z*z));      //сalculate leght of side figure;
-			x=0; y=0; z=0;
+			L12 = sqrt((x*x) + (y*y) + (z*z)); //Calculate leght of side figure;
+			x=0; y=0; z=0;  
 
 			x = (Figure->points[2].x) - (Figure->points[3].x);
 			y = (Figure->points[2].y) - (Figure->points[3].y);
 			z = (Figure->points[2].z) - (Figure->points[3].z);
 			L23 = sqrt((x*x) + (y*y) + (z*z));
-			x=0; y=0; z=0;
+			x=0; y=0; z=0;  
 
 			x = (Figure->points[2].x) - (Figure->points[5].x);
 			y = (Figure->points[2].y) - (Figure->points[5].y);
