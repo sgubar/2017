afef0b692e53d954c75fa3833061ccf8ba31e9a2
MaksGolub <maks.golub.1991@gmail.com>
2017-04-04 22:12:04 +0300

    Update main.c

--- dk62/MaksGolub/HW2/main.c
+++ dk62/MaksGolub/HW2/main.c
@@ -5,11 +5,12 @@
 
 int main(void)
 {   
-    printf("Enter a quantity of Figure:");
-	int aSize=scan_f();
-	FigureList *List=createFigureList(aSize);
+    
+    printf("Enter a quantity of Figure (not more 10):");
+	int Size=scan_f();
+	FigureList *List=createFigureList(10);
 	int i;
-	for(i=0; i<aSize; i++)
+	for(i=0; i<Size; i++)
     {
     addCoor(List);
     }
