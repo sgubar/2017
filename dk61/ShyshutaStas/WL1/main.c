bc077b14ca1cbee80b0ab710fc6f637a660a2866
ShyshutaStas <3p141592stas@gmail.com>
2017-04-04 10:57:54 +0300

    Update main.c

--- dk61/ShyshutaStas/WL1/main.c
+++ dk61/ShyshutaStas/WL1/main.c
@@ -3,19 +3,21 @@
 
 int main(int argc, char *argv[]) 
 
-{
+{   
+	//declare variables
 	int result = 0;
 	int F = 0, D = 0, C = 0;
-
-	printf("\nenter First nomber:");
+	//enter values
+	printf("\n enter First nomber:");
 		scanf("%d", &D) ;
-	printf(" \nSecond nomber:");
+	printf(" \n Second nomber:");
 		scanf ("%d",&C);
-	printf("\nenter nomber on hex sistem :");
+	printf("\n enter nomber on hex sistem :");
 		scanf("%x", &F) ;
 	
-	
+	//call feature
     result = sum_series(D , C , F);
+	//output value
     printf("\nresult: %d",result);
 	return 0;
 }
