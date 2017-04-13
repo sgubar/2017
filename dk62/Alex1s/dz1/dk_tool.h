c7dfd82d431816bd0003331deb11b0203ba7d995
dest1nyd1spers1on <alexafa@ukr.net>
2017-04-13 23:21:09 +0300

    Update dk_tool.h

--- dk62/Alex1s/dz1/dk_tool.h
+++ dk62/Alex1s/dz1/dk_tool.h
@@ -3,7 +3,7 @@
 
 int enter_value();
 int write_matrix(int lines, int columns, float **matrix);
-int mnoj_matrix(int coef, int lines, int columns, float **matrix);
+int mnoj_matrix(int lines, int columns, float **matrix);
 int read_matrix(int lines, int columns, float **matrix);
 
 #endif
