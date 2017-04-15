e78b325074bd6bfc605d89a832ff48d664c88a23
Llokos <Llokos@meta.ua>
2017-04-15 15:56:41 +0300

    Create main.c

---
+++ dk62/SokolYaroslav/homework3/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h> 
+#include "hw3dk_tool.h"
+#include "hw3dk_tool.c"
+int main(void)
+{   
+    printf("Ведіть кількість фігур:");
+	int n=inputint(4);
+	spisok_figur *spisok=new_spisok_figur(9999);
+	for(int i=0; i<n; i++)
+    {
+    add_сoord(spisok);
+    }
+	print_figura(spisok);
+	FILE *file=fopen("newfile.json", "w");
+	printinfile_spisok(file, spisok);
+	fflush(file);
+	fclose(file);
+	delete_spisok_figur(spisok);
+}
