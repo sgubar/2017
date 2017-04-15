5dc9cb04a04b7eb5d8efe996bd7f88c1c7b520b5
Llokos <Llokos@meta.ua>
2017-04-15 15:57:04 +0300

    Create hw3dk_tool.h

---
+++ dk62/SokolYaroslav/homework3/hw3dk_tool.h
@@ -0,0 +1,26 @@
+#ifndef hw3dk_tool_h
+#define hw3dk_tool_h
+typedef struct coordinate
+{
+	int x;
+	int y; 
+}tochka;
+typedef struct tochki
+{
+	tochka *tochki; 
+}figura;
+typedef struct spisok
+{
+	figura *quadrangle; 
+	int N;
+	int Flag;
+}spisok_figur;
+spisok_figur *new_spisok_figur(int maxreal); 		//створення структури
+int add_сoord(spisok_figur *myfigura);		//додавання коодинат до структури
+void delete_spisok_figur(spisok_figur *myfigura);		//видалення структури
+void print_figura(spisok_figur *myfigura);		//друк структури
+void printinfile_spisok(FILE *file, spisok_figur *myfigura);		//друк списка в файл JSON
+void printinfile_figura(FILE *file, figura *myfigura);
+int inputint(int limit);		//введення та перевірка числа
+float Ploshcha(figura *figura);		//знаходження площі чотирикутника
+#endif
