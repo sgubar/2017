79f64d7bbd9c8a8fdf0df8a38661603db25d0546
AShashkov <andr-sh@ukr.net>
2017-04-19 08:59:13 +0300

    Add files via upload

---
+++ dk62/Andrey_Shashkov/hw3/dk_tool.h
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#ifndef dk_tool_h
+#define dk_tool_h
+
+typedef struct coordinate
+{
+	int x,y,z;	
+}Point;
+
+typedef struct Points
+{
+	Point *points;
+}Figure;
+
+typedef struct List
+{
+	Figure *paralel; 
+	int size,current_size;
+}mylist;
+
+mylist *createMyList(int size);
+int addCoor(mylist *aFigure);
+void printMyFigure(mylist *aFigure);
+void destroyMyList(mylist *aFigure);
+void writeMyList(FILE *aFile, mylist *aFigure); 
+void writeMyFigure(FILE *aFile, Figure *aFigure);
+int scan(void);
+
+
+#endif 
