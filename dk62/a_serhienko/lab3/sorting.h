161452b9a7395d7771854b54c50c9a967b470c49
RTshok <fireart29@gmail.com>
2017-04-21 13:32:29 +0300

    Delete sorting.h

--- dk62/a_serhienko/lab3/sorting.h
+++
@@ -1,9 +0,0 @@
-#ifndef sorting_h
-#define sorting_h
-#include <stdio.h>
-
-void ShellSort(char * array, int size);
-void BubbleSort(char * array, int size);
-void SelectionSort(char * array,int size);
-void InsertionSort(char * array, int size);
-#endif
