7b67f3b92b43bc2f133a0b3631b95a3e48434249
Vihot <mvihot2@gmail.com>
2017-04-25 00:37:01 +0300

    Add files via upload

--- dk62/Vihot/dk_tools.h
+++ dk62/Vihot/dk_tools.h
@@ -1,16 +1,25 @@
-
-
-float modulAB= sqrt(xAB*xAB+yAB*yAB);
-float modulBC= sqrt(xBC*xBC+yBC*yBC);
-float modulCD= sqrt(xCD*xCD+yCD*yCD);
-float modulDA=sqrt(xDA*xDA+yDA*yDA);
-
-float ABDA=xAB*xDA+yAB*yDA;
-float BCCD=xBC*xCD+yBC*yCD;
- 
-float A = ABDA/modulAB*modulDA;
-float C = BCCD/	modulBC*modulCD;
-float p = (modulAB+modulBC+modulCD+modulDA)/2;
-float S = sqrt((p -modulAB )*(p-modulBC)*(p-modulCD)*(p-modulDA)-modulAB*modulBC*modulCD*modulDA*(A*C)/2);
-//Основні функції використані в коді.
-
+#ifndef dk_tools_h
+#define dk_tools_h
+typedef struct coordinate
+{
+	int x;
+	int y; 
+}tochka;
+typedef struct tochki
+{
+	tochka *tochki; 
+}figura;
+typedef struct spisok
+{
+	figura *quadrangle; 
+	int N;
+	int Flag;
+}spisok_figur;
+spisok_figur *spisok_figur(int maxreal); 		
+int add_coord(spisok_figur *figura);		
+void delete_spisok_figur(spisok_figur *figura);	
+void print_figura(spisok_figur *figura);		
+void printinfile_spisok(FILE *file, spisok_figur *figura);		
+void printinfile_figura(FILE *file, figura *figura);
+int inputint(int limit);	
+#endif
