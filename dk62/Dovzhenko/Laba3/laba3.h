2ef38ad793cbe06dcfaf43362a708acac0bf6fe0
DovzhenkoD <d.dovgenko@gmail.com>
2017-05-04 11:25:11 +0300

    Create laba3.h

---
+++ dk62/Dovzhenko/Laba3/laba3.h
@@ -0,0 +1,12 @@
+#ifndef laba3_h
+#define laba3_h
+
+void swap(char *line, int LeftElem, int RightElem);
+void Print_To_File (int counter,char *string);
+int mediana(char *line, int LeftElem, int RightElem);
+int partitionIt(char *line, int LeftElem, int RightElem, char aPivot);
+void additionalsort(char *line, int LeftElem, int RightElem);
+void quickSort(char *line, int LeftElem, int RightElem);
+int BinarySearch( char *string, char find);
+
+#endif
