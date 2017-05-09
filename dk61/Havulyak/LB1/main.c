f7c3f951e8ab8dfb83e7a4dea944d0243ce091e9
Stanis209 <dante123321123@gmail.com>
2017-05-10 01:50:08 +0300

    Update main.c

--- dk61/Havulyak/LB1/main.c
+++ dk61/Havulyak/LB1/main.c
@@ -1,1 +1,18 @@
+//  main.c
+//  LB_1
+//  Created by Stanislav Havulyak on 24/5/17.
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(void)
+{
+	int A;
+	double B, C;
+	printf("Enter the A, B, C:\n");
+	scanf("%i""%lf""%lf",&A, &B, &C);
+	double finale = ((A+B+C)/(100-B-C))+(summ_with_pow(A));
+	printf("Result:");
+	printf("%lf", finale);
+	return 0;
+	
+}
