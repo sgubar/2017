8dda25832826033e8d666ea2bb728c90ddb21c62
Stanis209 <dante123321123@gmail.com>
2017-05-10 07:33:38 +0300

    Update dk_tool.c

--- dk61/Havulyak/Dz1/dk_tool.c
+++ dk61/Havulyak/Dz1/dk_tool.c
@@ -4,38 +4,43 @@
 
 #include "dk_tool.h"
 
-void create_matrix(int x, int y, int matr1[x][y], int matr2[x][y], int matr_otvet[x][y])
+int create_matrix(int *d, int *f)
 {	
-	do{
-		printf("Vvedite kolichestvo strok: ");
-		scanf("%i", &x);
-	}while(x<1);
-	do{
-		printf("Vvedite kolichestvo stolbtsov: ");
-		scanf("%i", &y);
-	}while(y<1);
 	int i=0;
 	int j=0;
-	for (i=0; i<x; i++)
+	for (i=0; i<*d; i++)
 	{  																							
-		for (j=0; j<y; j++)
-		{ 
+		for (j=0; j<*f; j++)
+		{   
+		    int matr1[i][j];
 			printf("Element matr1 [%i][%i]: ", i+1, j+1);	   
 			scanf("%i", &matr1[i][j]);				            
 		}
 	}
-	for (i = 0; i < x; i++)
+	for (i = 0; i < *d; i++)
 	{        				  
-		for(j = 0; j < y; j++)
-		{			   
+		for(j = 0; j < *f; j++)
+		{	
+			int matr2[i][j];	   
 			printf("Element matr2 [%i][%i]: ", i+1, j+1);	  
 			scanf("%i", &matr2[i][j]);	
 		}
 	}
-	for (i = 0; i < x; i++){       
-		for(j = 0; j < y; j++){	 
-		   matr_otvet[i][j]=matr1[i][j]-matr2[i][j];
-		   printf("[%2i]", matr_otvet[i][j]);				      
-		}
-	}
+	int *matrx1, *matrx2;
+	matrx1=matr1;
+	matrx2=matr2;
+}
+
+
+int substraction_matrix(int *matr1, int *matr2, int *d, int *f)
+{
+	
+	int i,j;
+	int matr_otvet[i][j];
+	for (i = 0; i < *d; i++){       
+		for(j = 0; j < *f; j++){	 
+		   matr_otvet[i][j]=(*matrx1)-(*matrx2);
+		   printf("[%2i]", matr_otvet[i][j]);			
+        }
+    }
 }
