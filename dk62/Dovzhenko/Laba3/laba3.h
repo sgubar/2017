4c9e18b6684c8ac67471ccdcb061125c34fb9756
DovzhenkoD <d.dovgenko@gmail.com>
2017-05-12 18:46:45 +0300

    Update laba3.h

--- dk62/Dovzhenko/Laba3/laba3.h
+++ dk62/Dovzhenko/Laba3/laba3.h
@@ -1,12 +1,16 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #ifndef laba3_h
 #define laba3_h
 
 void swap(char *line, int LeftElem, int RightElem);
-void Print_To_File (int counter,char *string);
-int mediana(char *line, int LeftElem, int RightElem);
-int partitionIt(char *line, int LeftElem, int RightElem, char aPivot);
-void additionalsort(char *line, int LeftElem, int RightElem);
-void quickSort(char *line, int LeftElem, int RightElem);
-int BinarySearch( char *string, char find);
+int BinarySearch(char *line, char find);
+void quicksort(char *line, int LeftElem, int RightElem);
+int LookFile(FILE *afile);
+void ReadFile(FILE *afile, char *line);
 
 #endif
+
