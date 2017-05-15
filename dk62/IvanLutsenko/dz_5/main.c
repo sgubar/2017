16678a46a1e1777c45eadc73311fa4f5b269665a
I-Lutsenko <alykard10@gmail.com>
2017-05-15 22:41:47 +0300

    Add files via upload

---
+++ dk62/IvanLutsenko/dz_5/main.c
@@ -0,0 +1,36 @@
+#include "dk_tool.h"
+#include <string.h>
+
+int main() 
+{
+	circle* array = NULL;
+	int size = 0, i;
+	do
+	{
+		printf("Set new circle`s x, y and radius (r = 0 to end):\n");
+		int x, y, r;
+		scanf("%i %i %i", &x, &y, &r);
+		if (r == 0)
+			break;		
+		if (init(&array, &size, x, y, r) == 1) 
+		{
+			printf("ERROR\n");
+			return 1;
+		}
+	}while(1);
+	output("unsortedShell.json", array, size);
+	
+	printf("Set radius to find: ");
+	scanf("%i", &i);
+	i = find(array, size, i);
+	if (i == -1)
+		printf("No element with such radius found\n");
+	else 
+		printf("Element with such radius has index %i\n", i);
+	
+	sortShell(array, size);
+	output("sortedShell.json", array, size);
+	
+	free(array);
+	return 0;
+}
