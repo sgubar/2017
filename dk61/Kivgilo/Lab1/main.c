c916425dd576b589fb8148c37b84046783b94dd3
Moskito1902 <kivgilov1902@gmail.com>
2017-05-16 02:10:54 +0300

    Update main.c

--- dk61/Kivgilo/Lab1/main.c
+++ dk61/Kivgilo/Lab1/main.c
@@ -1,24 +1,49 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include "tool.h" 
 
-int main(int argc, char *argv[]) 
+int main()
 {
-	int A;
-	int C;
-	int B;
-	printf ("A = ");
-	scanf ("%i", &A);
-	printf ("\nB = ");
-	scanf ("%i", &B);
-	printf ("\nC = ");
-	scanf ("%i", &C);
-	float D;
-	float D1 = dobutok( A, B, C);
-	printf ("\n");
-	printf ("D1= %.2f \n",D1);
-	int D2 = sum(B);
-	printf ("D2= %i\n", D2);
-	D= D1+D2;
-	printf ("D= %.2f\n", D);
-	return 0;
+    float X=0; 
+    float Y=0; 
+    float Q=0; 
+    float A,C; 
+    float B; 
+    float D=0; 
+    float sum; 
+
+    printf("A=");
+    scanf("%f",&A); 
+    printf("\n");
+
+    printf("B=");
+    scanf("%f",&B); 
+    printf("\n");
+
+    printf("C=");
+    scanf("%f",&C); 
+    printf("\n");
+
+    
+   
+     if (B + sqr(C) == 0 && B <= 0) 
+        {
+         printf(" Vvedi chislo > 0 \n"); 
+         return 0;
+        }
+    else
+        {
+            X=(A*B)/(B+sqr(C)); 
+            printf("X=%f",(A*B)/(B+sqr(C))); 
+        }
+    printf("\n");
+    
+    Y=suma_factorialov(D,B); 
+    
+    printf("Y=%f",suma_factorialov(D,B)); 
+    printf("\n");
+
+    Q=X+Y;
+    printf("Q=%f",X+Y);
 }
+
+
