939a632b6c0c55a30bed9eda539f85228456a41f
MaksGolub <maks.golub.1991@gmail.com>
2017-05-15 22:29:39 +0300

    Add files via upload

---
+++ dk62/MaksGolub/HW5/tool.h
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define tool_h
+
+typedef struct coordinate
+{
+	int x;
+	int y; 
+	int z;
+	
+}Point;
+
+typedef struct Points
+{
+	Point *points; //pointer on coordinate;
+}Figure;
+
+typedef struct List
+{
+	Figure *paralel; //pointer on points;
+	int size;
+	int current_size;
+	
+}FigureList;
+
+FigureList *createFigureList(int size);
+int addCoor(FigureList *aFigure);
+int addCoordinate();
+void printFigure(FigureList *aFigure);
+void destroyFigureList(FigureList *aFigure);
+void writeList(FILE *aFile, FigureList *aFigure); //write List in JSON;
+void writeFigure(FILE *aFile, Figure *aFigure); //Auxiliary funcion to write points;
+void BubbleSort(FigureList *aFigure); //function of bubble sorting;
+int BinarySearch(FigureList *aFigure); //function of binary searching;
+void swap(FigureList *aFigure, int aLeftIndex, int aRightIndex);
+int mediana(FigureList *aFigure, int aLeftIndex, int aRightIndex);
+int m_partitionIt(FigureList *aFigure, int aLeftIndex, int aRightIndex, int aPivot);
+void quickSort2(FigureList *aFigure, int aLeftIndex, int aRightIndex);
+void m_manualSort(FigureList *aFigure, int aLeftIndex, int aRightIndex);
+int scan_f(void);
