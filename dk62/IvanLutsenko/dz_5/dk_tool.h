8d6ae6a92b652fcc48563ee110f166e1b9521b04
I-Lutsenko <alykard10@gmail.com>
2017-05-15 22:40:01 +0300

    Add files via upload

---
+++ dk62/IvanLutsenko/dz_5/dk_tool.h
@@ -0,0 +1,25 @@
+#ifndef _DK_TOOL_H
+#define _DK_TOOL_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#define pi 3.14
+
+typedef struct {
+	int x;
+	int y;
+	int rad;
+} circle;
+
+double square(circle c);
+
+char init(circle **array, int *n, int x, int y, int rad);
+
+char output(char *path, circle *array, int n);
+
+void sortShell(circle array[], int n);
+
+int find(circle array[], int n, int x);
+
+#endif
