73b31f03c402f2bd7f9a2d7b4bc55198659399b1
Kufaeva2103 <sasha99549@gmail.com>
2017-05-16 21:18:15 +0300

    Create dk_tool.h

---
+++ dk62/Kufaeva_Sasha/Homework/lab3/dk_tool.h
@@ -0,0 +1,8 @@
+#ifndef dk_tool_h
+#define dk_tool_h
+
+void values(int *aArray, int aSize);
+void sortArray(int *aArray, int aSize, FILE *file);
+void searchArray(int *aArray, int aSize, FILE *file);
+
+#endif
