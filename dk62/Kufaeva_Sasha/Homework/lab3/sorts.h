92dafd6ad5887dce92f0bffa9f97fd060a76fdcf
Kufaeva2103 <sasha99549@gmail.com>
2017-05-16 21:19:37 +0300

    Create sorts.h

---
+++ dk62/Kufaeva_Sasha/Homework/lab3/sorts.h
@@ -0,0 +1,14 @@
+#ifndef sort_h
+#define sort_h
+
+void bubblesort(int *aArray, int aSize);
+void selectionsort(int *aArray, int aSize);
+void insertionsort(int *aArray, int aSize);
+void shellsort(int *aArray, int aSize);
+void quicksort(int *aArray, int first, int last);
+void swap(int *tmp1, int *tmp2);
+
+int linesearch(int *aArray, int aSize, int val);
+int binarysearch(int *aArray, int aSize, int val);
+
+#endif
