0398a1a27707029e6fafd91214e8cbe55481b6e7
Bogdan Ponomarenko <pb6798@gmail.com>
2017-05-16 03:33:54 +0300

    Create t.h

---
+++ dk61/bogdanponomarenko/HW2/t.h
@@ -0,0 +1,40 @@
+#ifndef t_h
+#define t_h
+
+
+typedef struct Coordinate
+{
+    int x;
+    int y;
+} RectanglePoint;
+
+typedef struct describe_1_rectangle
+{
+    RectanglePoint * PointA;
+    RectanglePoint * PointB;
+    RectanglePoint * PointC;
+    RectanglePoint * PointD;
+} Rectangle;
+
+typedef struct RectangleList
+{
+    Rectangle* Rectangles;
+    int size;
+    int current_size;
+} ListFigures;
+
+ListFigures *CreateListFigure (int aSize);
+
+int AddRectangle(ListFigures * AFigures);
+
+double Square(Rectangle * rectangle);
+
+void PrintSquare(ListFigures *FiguresNumber);
+
+void DestroyList(ListFigures * AFigures);
+
+void Coordinates(RectanglePoint*PointA, RectanglePoint*PointB, RectanglePoint*PointC, RectanglePoint*PointD);
+
+
+
+#endif
