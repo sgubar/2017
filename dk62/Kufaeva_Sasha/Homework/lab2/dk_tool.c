d9024ed5245b08dcc35652a4a7317ee64fc163fc
Kufaeva2103 <sasha99549@gmail.com>
2017-05-16 21:12:41 +0300

    Create dk_tool.c

---
+++ dk62/Kufaeva_Sasha/Homework/lab2/dk_tool.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "dk_tool.h"
+#include "sorts.h"
+
+void sortArray(int *aArray, int aSize, FILE *file)
+{
+	clock_t first, second;
+	double time;
+	
+	values(aArray, aSize);
+	
+	printf("Bubblesort started\n");
+	first = clock();
+	bubblesort(aArray, aSize);
+	second = clock();
+	
+	time = (second - first)/1000;
+	printf("Bubblesort finished\n");
+	fprintf(file, "Bubblesort\t%.5lf s\n", time);
+	
+	
+	values(aArray, aSize);
+	
+	printf("Insertionsort started\n");
+	first = clock();
+	insertionsort(aArray, aSize);
+	second = clock();
+	
+	time = (second - first)/1000;
+	printf("Insertionsort finished\n");
+	fprintf(file, "Insertionsort\t%.5lf s\n", time);
+	
+	
+	values(aArray, aSize);
+	
+	printf("Selectionsort started\n");
+	first = clock();
+	selectionsort(aArray, aSize);
+	second = clock();
+	
+	time = (second - first)/1000;
+	printf("Selectionsort finished\n");
+	fprintf(file, "Selectionsort\t%.5lf s\n", time);
+	
+	
+	values(aArray, aSize);
+	
+	printf("Shellsort started\n");
+	first = clock();
+	shellsort(aArray, aSize);
+	second = clock();
+	
+	time = (second - first)/1000;
+	printf("Shellsort finished\n");
+	fprintf(file, "Shellsort\t%.5lf s\n", time);
+	
+	
+	values(aArray, aSize);
+	
+	printf("Quicksort started\n");
+	first = clock();
+	quicksort(aArray, 0, aSize - 1);
+	second = clock();
+	
+	time = (second - first)/1000;
+	printf("Quicksort finished\n");
+	fprintf(file, "Quicksort\t%.5lf s\n", time);
+}
+
+void values(int *aArray, int aSize)
+{
+	srand(time(NULL));
+	
+	for(int i = 0; i < aSize; i++)
+		aArray[i] = rand();
+}
