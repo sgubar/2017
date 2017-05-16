664b17de7c75f554f650ff24e2e8ccc2debdf8b1
I-DefenDer <mhuhbjp@yandex.ua>
2017-05-17 02:54:52 +0300

    Delete dk_tool.h

--- dk62/Kiriluk_Vlad/dk_tool.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-void BubbleSort ( char *buffer , int length);
-void InsertionSort ( char *buffer , int length);
-void SelectionSort(int length , char *buffer);
