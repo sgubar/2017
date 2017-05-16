88f944ed4514a108c1cca6e632105e36641aaaee
Goordey <Goordey1998@gmail.com>
2017-05-17 02:38:11 +0300

    Add files via upload

---
+++ dk62/Yaroslav -_- Hordienko/home_work 2/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "tool.h"
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+ figure *acharfigure = {0};
+ float x = ploscha (acharfigure);
+	printf("Ploscha kola = %f", x);
+}
+
