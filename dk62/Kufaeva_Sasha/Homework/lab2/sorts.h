df88df37fee1769deeab2040fe77e1a4e2330182
Kufaeva2103 <sasha99549@gmail.com>
2017-05-17 09:04:26 +0300

    Update sorts.h

--- dk62/Kufaeva_Sasha/Homework/lab2/sorts.h
+++ dk62/Kufaeva_Sasha/Homework/lab2/sorts.h
@@ -4,8 +4,6 @@
 void bubblesort(int *aArray, int aSize);
 void selectionsort(int *aArray, int aSize);
 void insertionsort(int *aArray, int aSize);
-void shellsort(int *aArray, int aSize);
-void quicksort(int *aArray, int first, int last);
 void swap(int *tmp1, int *tmp2);
 
 #endif
