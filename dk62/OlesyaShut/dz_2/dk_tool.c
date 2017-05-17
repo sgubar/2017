0a223dc4a8db01ae28349cfd985656ba91be2c47
Olesya <olesya.shut@mail.ru>
2017-05-17 06:44:49 +0300

    Update dk_tool.c

--- dk62/OlesyaShut/dz_2/dk_tool.c
+++ dk62/OlesyaShut/dz_2/dk_tool.c
@@ -1,48 +1,80 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "dk_tool.h"
 
 #define M_PI 3.14159265358979323846
 
-
-koordunatu* xy_koordunatu()
-
-
-
-void enter(koordunatu *AA)
+list *createList(int aSize)
 {
-	if(NULL != AA)
+    list *aList = (list *)malloc(sizeof(list));
+    if(aList != NULL)
 	{
-		printf("Enter x koordinate: ");
-		scanf("%f",&AA->x_koordunatu);
-		
-		printf("\nEnter y koordinate: ");
-		scanf("%f",&AA->y_koordunatu);
-		
-		AA->radius = proverka();
+		aList->circle = (coordinates *)calloc(aSize, sizeof(coordinates));
+		aList->maxSize = aSize;
+		aList->currentSize = 0;
+	}
+	return aList;
+}
+
+
+void destroyList(list *List)
+{
+	if(NULL != List)
+	{
+		if(NULL != List->circle)
+		{
+			free(List->circle);
+		}
+		free(List);
 	}
 }
 
 
-float proverka()
+void newCoordinate(list *aList)
 {
-	float rez;
-	do{
-		printf("\nEnter radius : ");
-		scanf("%f", &rez);
-	}while(rez <= 0);
+	if(NULL != aList)
+	{
+		if(aList->currentSize < aList->maxSize)
+		{
+			coordinates *aCurrent = &(aList->circle[aList->currentSize]);
+			printf("Enter x koordinate: ");
+			scanf("%f", &aCurrent->x);
+			
+			printf("Enter y koordinate: ");
+			scanf("%f", &aCurrent->y);
+			do{
+				printf("Enter radius : ");
+				scanf("%f", &aCurrent->r);
+			}while(aCurrent->r < 0);
 
-	return rez;
+			printf("\nResult: %.12f\n\n", square(aCurrent));
+			aList->currentSize++;
+		}
+	}
 }
 
 
-float calculate(koordunatu *AAA)
+float square(coordinates *current)
 {
-	float rezultat = M_PI * AAA->radius * AAA->radius;
+	float result = M_PI * current->r * current->r;
+	
+	if(result < 0) 
+		result *= -1;
 
-	if(rezultat<0) 
-		rezultat *= (-1);
+	return result;
+}
 
-	return rezultat; 
+void printArea(list *aList)
+{
+	if(NULL != aList)
+	{
+		int i;
+		for(i = 0; i < aList->currentSize; i++)
+		{
+			coordinates *aCurrent = &(aList->circle[i]);
+			printf("Circle[%i]: \"X\":%.3f", i, aCurrent->x);
+			printf(" \"Y\":%.3f \"Radius\":%.3f", aCurrent->y, aCurrent->r);
+			printf(" \"Square\":%.3f\n", square(aCurrent));
+		}
+	}
 }
