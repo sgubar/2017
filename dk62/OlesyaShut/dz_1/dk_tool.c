31fa95fecb793cc9204d65d6fb633e12e612eb74
Olesya <olesya.shut@mail.ru>
2017-05-17 06:06:59 +0300

    Update dk_tool.c

--- dk62/OlesyaShut/dz_1/dk_tool.c
+++ dk62/OlesyaShut/dz_1/dk_tool.c
@@ -2,24 +2,28 @@
 #include <string.h>
 #include "dk_tool.h"
 
-int sea(char *str, char *search)
- {
-	int result = 0, i = 0, j = 0;
-	while(i < strlen(str))
-    	{
-		if(str[i]==search[j])
-		  {
-			do{
+int search(char *str, char *txt)
+{
+	int result = 0, i = 0, j = 0;			
+	while(i < strlen(str))					
+	{
+		if(str[i] == txt[j])				
+		{
+			do{								
 				i++;
 				j++;
-				if(j == strlen(search)-1)
+				if(j == strlen(txt) - 1)
+				{
 					result++;
-			  }while(str[i]==search[j]&&str[i]!='\0');
-	     	j = 0;
+				}
+			}while(str[i] == txt[j] && str[i] != '\0');	
+			
+			j = 0;							
 		}
-    
-    	else
-    	i++;
-	  } 
-	return result;
+		else
+		{
+			i++;
+		}
+	}
+return result;							
 }
