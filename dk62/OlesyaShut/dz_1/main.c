a639aa6c1190e2645e50b97264f2883fab109f65
Olesya <olesya.shut@mail.ru>
2017-05-17 06:11:45 +0300

    Update main.c

--- dk62/OlesyaShut/dz_1/main.c
+++ dk62/OlesyaShut/dz_1/main.c
@@ -2,23 +2,24 @@
 #include <stdlib.h>
 #include "dk_tool.h"
 
-int main(int argc, char *argv[]) {
-	char *str = (char *) malloc(sizeof(char) * 100);
-	char *search = (char *) malloc(sizeof(char) * 50);
-	int result = 0;
+int main(int argc, char *argv[])
+{
+char *str = (char *) malloc(sizeof(char) * 100);	
+char *txt = (char *) malloc(sizeof(char) * 15);		
+int result = 0;
 
-	printf("Enter stroku\n");
-	fgets(str, 100, stdin);
-	
-	printf("Enter word to search\n");
-	fgets(search, 50, stdin);
-	
-	result = sea(str, search);
-	printf("%i\n", result);
-	
-	free(search);
-	free(str);
-	
-	system("pause");	
-	return 0;
+printf("Enter text:\n");							
+fgets(str, 100, stdin);								
+
+printf("Enter word to search:\n");					
+fgets(txt, 15, stdin);								
+
+result = search(str, txt);							
+printf("%i\n", result);					
+
+free(str);											
+free(txt);											
+
+system("pause");	
+return 0;
 }
