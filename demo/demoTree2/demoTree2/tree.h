57e9eda38013f2c9afc137ce781aba56fbeae68d
Jayawardenepura <kuzhilnyioleg16@gmail.com>
2017-05-19 00:48:17 +0300

    Update tree.h

--- demo/demoTree2/demoTree2/tree.h
+++ demo/demoTree2/demoTree2/tree.h
@@ -1,48 +1,41 @@
 //
-//  tree.h
-//  demoTree2
-//
-//  Created by Slava Gubar on 4/25/17.
-//  Copyright © 2017 Slava Gubar. All rights reserved.
-//
-
-#ifndef tree_h
-#define tree_h
-
-#include <stdio.h>
-
-typedef struct __tagNode
-{
-	double value;
-	struct __tagNode *leftChild;
-	struct __tagNode *rightChild;
-} Double_Node;
-
-typedef struct __tagTree
-{
-	Double_Node *root;
-	int count;
-} DoubleTree;
-
-// interface
-DoubleTree *createDoubleTree();
-void destroyDoubleTree(DoubleTree *aTree);
-
-void insertDoubleValueToTree(DoubleTree *aTree, double aValue);
-
-Double_Node *findNodeWithValue(DoubleTree *aTree, double aValue);
-void deleteNodeWithValue(DoubleTree *aTree, double aValue);
-
-void mergeTrees(DoubleTree *aTreeDst, DoubleTree *aTreeSrc);
-
-//void print (Double_Node *t,int u);
-
-int countNodesWithTree(DoubleTree *aTree);
-
-void printDoubleTree(DoubleTree *aTree, int aTypeOfPassage);
-
-void SymmetricPassage_And_PrintTree(Double_Node *aNode);
-void PlainPassage_And_PrintTree(Double_Node *aNode);
-void BackPassage_And_PrintTree(Double_Node *aNode);
-
-#endif /* tree_h */
+ +//  tree.h
+ +//  demoTree2
+ +//
+ +//  Created by Slava Gubar on 4/25/17.
+ +//  Copyright © 2017 Slava Gubar. All rights reserved.
+ +//
+ +
+ +#ifndef tree_h
+ +#define tree_h
+ +
+ +#include <stdio.h>
+ +
+ +typedef struct __tagNode
+ +{
+ +	short value;
+ +	struct __tagNode *leftChild;
+ +	struct __tagNode *rightChild;
+ +} ShortNode;
+ +
+ +typedef struct __tagTree
+ +{
+ +	ShortNode *root;
+ +	int count;
+ +} ShortTree;
+ +
+ +// interface
+ +ShortTree *createShortTree();
+ +void destroyShortTree(ShortTree *aTree);
+ +
+ +void insertShortValueToTree(ShortTree *aTree, short aValue);
+ +
+ +ShortNode *findNodeWithValue(ShortTree *aTree, short aValue);
+ +void deleteNodeWithValue(ShortTree *aTree, short aValue);
+ +
+ +void mergeTrees(ShortTree *aTreeDst, ShortTree *aTreeSrc);
+ +
+ +void printTree(ShortTree *aTree);
+ +int countNodesWithTree(ShortTree *aTree);
+ +
+ +#endif /* tree_h */
