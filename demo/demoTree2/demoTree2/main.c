820bc2e34c6362c074e72e39af043d16a6dbb5ee
Jayawardenepura <kuzhilnyioleg16@gmail.com>
2017-05-19 00:50:05 +0300

    Sorry :)

--- demo/demoTree2/demoTree2/main.c
+++ demo/demoTree2/demoTree2/main.c
@@ -1,34 +1,15 @@
 //
-//  main.c
-//  demoTree2
-//
-//  Created by Slava Gubar on 4/25/17.
-//  Copyright © 2017 Slava Gubar. All rights reserved.
-//
-
-#include <stdio.h>
-#include "tree.h"
-
-
-int main(int argc, const char * argv[]) {
-	
-    
-    DoubleTree *TheTree = createDoubleTree();
-    
-    insertDoubleValueToTree(TheTree,  2.63);
-    insertDoubleValueToTree(TheTree,  5.6);
-    insertDoubleValueToTree(TheTree,  2.6);
-    insertDoubleValueToTree(TheTree,  9.63);
-    insertDoubleValueToTree(TheTree,  0.1);
-    insertDoubleValueToTree(TheTree,  3.63);
-    insertDoubleValueToTree(TheTree,  9.1);
-    insertDoubleValueToTree(TheTree,  9.3);
-    insertDoubleValueToTree(TheTree,  2.63);
-    insertDoubleValueToTree(TheTree,  1.63);
-
-     printDoubleTree (TheTree,1);
-
-    
-    
-	return 0;
-}
+ //  main.c
+ //  demoTree2
+ //
+ //  Created by Slava Gubar on 4/25/17.
+ //  Copyright © 2017 Slava Gubar. All rights reserved.
+ //
+ 
+ #include <stdio.h>
+ 
+ int main(int argc, const char * argv[]) {
+ 	// insert code here...
+ 	printf("Hello, World!\n");
+ 	return 0;
+ }
