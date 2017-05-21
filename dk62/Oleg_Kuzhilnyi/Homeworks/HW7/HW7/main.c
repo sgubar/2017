977df4cee83aaaf975bb385280197f6d33ebbbb4
Jayawardenepura <kuzhilnyioleg16@gmail.com>
2017-05-21 14:43:46 +0300

    Update main.c

--- dk62/Oleg_Kuzhilnyi/Homeworks/HW7/HW7/main.c
+++ dk62/Oleg_Kuzhilnyi/Homeworks/HW7/HW7/main.c
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 
 #include <stdio.h>
 #include "tree.h"
@@ -46,20 +44,3 @@ int main(int argc, const char * argv[]) {
     
 	return 0;
 }
-=======
-//
- //  main.c
- //  demoTree2
- //
- //  Created by Slava Gubar on 4/25/17.
- //  Copyright © 2017 Slava Gubar. All rights reserved.
- //
- 
- #include <stdio.h>
- 
- int main(int argc, const char * argv[]) {
- 	// insert code here...
- 	printf("Hello, World!\n");
- 	return 0;
- }
->>>>>>> origin/master
