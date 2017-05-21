e3042d3baeac11036c570f5377acd784d927074c
Vitaliy Silchuk <nubb99@gmail.com>
2017-05-21 04:35:07 +0300

    Update tool.h

--- dk61/vsilchuk/HW2/tool.h
+++ dk61/vsilchuk/HW2/tool.h
@@ -1,29 +1,29 @@
 #ifndef tool_h
 #define tool_h
 
-typedef struct pyramidVertex				//вершина
+typedef struct pyramidVertex				
 {
 	int x;
 	int y;
 	int z;	
 }vertex;
 
-typedef struct pyramidFigure			//пирамида
+typedef struct pyramidFigure			
 {
 	vertex *vertices;	
 }pyramid;
 
-typedef struct figuresList					//комната
+typedef struct figuresList					
 {
 	pyramid *pyramids;
 	int maxSize;
 	int pyramidsCounter;
 }pyramidsList;
 
-pyramidsList *createPyramidsList (int aSize);				//создали комнату для пирамид
-void destroyAPyramidsList(pyramidsList *aPyramidsList);		//удалили комнату с пирамидами
+pyramidsList *createPyramidsList (int aSize);				
+void destroyAPyramidsList(pyramidsList *aPyramidsList);		
 
-int addPyramid (pyramidsList *aPyramidsList);				//добавление пирамиды в комнату
+int addPyramid (pyramidsList *aPyramidsList);				
 
 void printPyramidsList(pyramidsList *aPyramidsList);
 int printPyramid(pyramid *aPyramid);	
