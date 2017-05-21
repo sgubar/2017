72c181045025305eddb00319d9a66a8f00ce0f90
Jayawardenepura <kuzhilnyioleg16@gmail.com>
2017-05-21 14:37:00 +0300

    Update tree.c

--- demo/demoTree2/demoTree2/tree.c
+++ demo/demoTree2/demoTree2/tree.c
@@ -1,4 +1,10 @@
-
+//
+//  tree.c
+//  demoTree2
+//
+//  Created by Slava Gubar on 4/25/17.
+//  Copyright © 2017 Slava Gubar. All rights reserved.
+//
 
 #include "tree.h"
 #include <stdlib.h>
@@ -106,9 +112,6 @@ ShortNode *findNodeWithValue(ShortTree *aTree, short aValue)
 	
 	return theCurrentNode;
 }
-<<<<<<< HEAD
-
-=======
 
 void deleteNodeWithValue(ShortTree *aTree, short aValue);
 
@@ -116,7 +119,6 @@ void mergeTrees(ShortTree *aTreeDst, ShortTree *aTreeSrc);
 
 void printTree(ShortTree *aTree);
 int countNodesWithTree(ShortTree *aTree);
->>>>>>> origin/master
 
 #pragma mark -
 void destroyNode(ShortNode *aNode)
@@ -143,145 +145,3 @@ ShortNode *createShortNodeWithValue(short aValue)
 	
 	return theNode;
 }
-<<<<<<< HEAD
-
-
-
-void printDoubleTree(DoubleTree *aTree, int aTypeOfPassage)
-{
-    if(NULL != aTree)
-    {
-        
-        printf("\t\t\t\t\t\tThe tree\n");
-        
-        switch(aTypeOfPassage)
-        {
-            case 1 :
-                
-                
-                SymmetricPassage_And_PrintNode(aTree->root);
-                putchar('\n');
-                
-                printf("The root: %.2f\n",aTree->root->value);
-
-        
-            break;
-    
-            case 2 :
-                
-                PlainPassage_And_PrintNode(aTree->root);
-                putchar('\n');
-                
-                printf("The root: %.2f\n",aTree->root->value);
-
-
-            break;
-                
-            case 3 :
-                
-                BackPassage_And_PrintNode(aTree->root);
-                putchar('\n');
-                
-                printf("The root: %.2f\n",aTree->root->value);
-
-                
-            break;
-        }
-    }
-}
-
-void BackPassage_And_PrintNode(Double_Node *aNode)
-{
-    if(NULL != aNode)
-    {
-        BackPassage_And_PrintNode(aNode->leftChild);
-        BackPassage_And_PrintNode(aNode->rightChild);
-        printf(" %.2f ",aNode->value);
-    }
-}
-
-void PlainPassage_And_PrintNode(Double_Node *aNode)
-{
-    if(NULL != aNode)
-    {
-        printf(" %.2f ",aNode->value);
-        PlainPassage_And_PrintNode(aNode->leftChild);
-        PlainPassage_And_PrintNode(aNode->rightChild);
-    }
-}
-
-
-void SymmetricPassage_And_PrintNode(Double_Node *aNode)
-{
-    if(NULL != aNode)
-    {
-        SymmetricPassage_And_PrintNode(aNode->leftChild);
-        printf(" %.2f ",aNode->value);
-        SymmetricPassage_And_PrintNode(aNode->rightChild);
-    }
-}
-
-
-Double_Node* FindMinNodeInBinarTree(Double_Node* root)
-{
-    while(root->leftChild != NULL) root = root->leftChild;
-    return root;
-}
-// Function to search a delete a value from tree.
-
-
- Double_Node* DeleteNodeFromTree(DoubleTree *aTree,Double_Node *Node, double data)
-
-{
-    if(Node == NULL) return Node;
-    
-    
-    else if(data < Node->value)
-        Node->leftChild = DeleteNodeFromTree(aTree,Node->leftChild,data);
-
-    
-    else if (data > Node->value)
-        Node->rightChild = DeleteNodeFromTree(aTree,Node->rightChild,data);
-
-    else {
-        
-        if(Node->leftChild == NULL && Node->rightChild == NULL) /*if no have children*/
-        {
-            free( Node);
-            Node = NULL;
-            
-            aTree->count--;
-        }
-        
-        //if have only one children
-    else if(Node->leftChild == NULL)        /*if have right children*/
-        {
-             Double_Node *temp = Node;
-            Node = Node->rightChild;
-            free( temp);
-            
-            aTree->count--;
-        }
-    else if(Node->rightChild == NULL)       /*if have left children*/
-        {
-             Double_Node *temp = Node;
-            Node = Node->leftChild;
-            free(temp);
-            
-           aTree->count--;
-        }
-        //if have 2 children
-    else
-        {
-             Double_Node *temp = FindMinNodeInBinarTree(Node->rightChild);
-            Node->value = temp->value;
-            Node->rightChild = DeleteNodeFromTree(aTree,Node->rightChild,temp->value);
-        }
-
-        aTree->count = aTree->count --;
-    }
-
-    return Node;
-}
-=======
->>>>>>> origin/master
