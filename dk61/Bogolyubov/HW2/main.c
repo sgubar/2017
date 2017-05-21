81ebabffaf47702337781c44611070dcda80f757
LIKIL <likil_thrasher@ukr.net>
2017-05-21 16:16:04 +0300

    Update main.c

--- dk61/Bogolyubov/HW2/main.c
+++ dk61/Bogolyubov/HW2/main.c
@@ -11,9 +11,9 @@ void main(void) {
 
 coord A; coord B; coord C; coord D;
 int value_of_squares;
-printf("Please, nter the number of pieces here: ");
+printf("Please, enter the number of pieces here: ");
 scanf("%i", &value_of_squares);
-float area, answer;
+float area;
 
 for(int i=0;i<value_of_squares;i++){
 printf("Enter x and y for point A here: "); // priem znacheniy x i y 
@@ -26,11 +26,12 @@ printf("Enter x and y for point D here: ");
 scanf("%i" "%i", &D.x, &D.y);
 area =((A.x*B.y)-(A.y*B.x)+(B.x*C.y)-(B.y*C.x)+(C.x*D.y)-(D.x*C.y)+(D.x*A.y)-(D.y*A.x))/2; // calculate area
 if(area < 0){ // proverka na otricaelnuyu ploshad 
-answer = area*(-1);
+area += area * (-1);
 }
-else
-area = answer;
-printf("Area of the polygon: %.2f\n", answer);
+else {
+
+printf("Area of the polygon: %.2f\n", area);
 printf("\n\n\n");
+}
 }
 }
