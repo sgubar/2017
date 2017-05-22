fd25d949f03507ae6a65993c3988eae7c63ef68a
Stanis209 <dante123321123@gmail.com>
2017-05-23 02:04:02 +0300

    Create main.c

---
+++ dk61/Havulyak/Dz4/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "tool.h"
+
+int main(void)
+{   
+    FILE *aFile=fopen("data.json", "w");
+    printf("Enter a quantity of Figure (not more 10):");
+	int Size=scan_f();
+	FigureList *List=createFigureList(10);
+	int i;
+	for(i=0; i<Size; i++)
+    {
+    addCoor(List);
+    }
+    printFigure(List); 
+    BubbleSort(List); 
+	printFigure(List); //print figures after sorting;
+	writeList(aFile, List);
+	
+	destroyFigureList(List);
+	
+}
