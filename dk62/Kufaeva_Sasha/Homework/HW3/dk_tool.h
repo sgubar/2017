cf9e5cbee1213286a25aca66219022b6b3f48b58
Kufaeva2103 <sasha99549@gmail.com>
2017-05-22 20:00:42 +0300

    Create dk_tool.h

---
+++ dk62/Kufaeva_Sasha/Homework/HW3/dk_tool.h
@@ -0,0 +1,33 @@
+#ifndef dk_tool_h
+#define dk_tool_h
+typedef struct coordinate
+{
+	
+	int xCoordinate;
+	int yCoordinate;
+	int zCoordinate;
+	
+}ParpipedCoordinate;
+
+typedef struct position
+{
+	ParpipedCoordinate *coordinates;
+	
+}ParpipedPoints;
+
+typedef struct List
+{
+	ParpipedPoints *parpipeds;
+	int size;
+	int current_size;
+	
+}ParpipedList;
+
+ParpipedList * createParpipedList(int aSize);
+void destroyParpipedList(ParpipedList *aParpiped);
+int addParpipedCoordinates(ParpipedList *aParpiped);
+void printParpipedList(ParpipedList *aParpiped);
+//int areaCalculation(ParpipedPoints * aParpiped);
+//int enterParametersOfSphere(void);
+
+#endif
