100348be60b6e665a05e65e1bd19c0fa0ebe638c
Kufaeva2103 <sasha99549@gmail.com>
2017-05-22 20:14:39 +0300

    Create sort.h

---
+++ dk62/Kufaeva_Sasha/Homework/HW4/sort.h
@@ -0,0 +1,11 @@
+#ifndef sort_h
+#define sort_h
+#include "dk_tool.h"
+void bubbleSort(ParpipedList *aParpiped);
+void selectionSort(ParpipedList *aParpiped);
+void insertionSort(ParpipedList *aParpiped);
+
+
+
+
+#endif
