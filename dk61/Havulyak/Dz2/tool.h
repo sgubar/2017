277c9070f00b95d546546758272e304ab01244f6
Stanis209 <dante123321123@gmail.com>
2017-05-23 02:00:35 +0300

    Create tool.h

---
+++ dk61/Havulyak/Dz2/tool.h
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define tool_h
+
+typedef struct coordinate
+{
+	int x;
+	int y; 
+	int z;
+	
+}Point;
+
+typedef struct Points
+{
+	Point *points; //pointer on coordinate;
+}Figure;
+
+typedef struct List
+{
+	Figure *paralel; //pointer on points;
+	int size;
+	int current_size;
+	
+}FigureList;
+
+FigureList *createFigureList(int size);
+int addCoor(FigureList *aFigure);
+void printFigure(FigureList *aFigure);
+void destroyFigureList(FigureList *aFigure);
+int scan_f(void);
