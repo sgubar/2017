d59deddfc4570e2481138b47e15719f83031f3ab
Olesya <olesya.shut@mail.ru>
2017-05-23 12:14:43 +0300

    Update main.c

--- dk62/OlesyaShut/dz_3/main.c
+++ dk62/OlesyaShut/dz_3/main.c
@@ -5,21 +5,21 @@
 int main(int argc, const char *argv[])
 {
 	int a = 0;
-	do{										//цикл будет выполняться, пока a < 1
+	do{										
 		printf("Enter number of circles: ");
 		scanf("%i", &a);
 	}while(a < 1);
 	
-	list *circ = createList(a);				//выделяет память на нужное количество кругов
+	list *circ = createList(a);				
 	
-	while(a != 0)							//цикл
+	while(a != 0)							
 	{
 		newCoordinate(circ);
 		a--;
 	}
 	
-	printArea(circ);						//выводит на экран
-	printJson(circ);						//выводит в файл
+	printArea(circ);						
+	printJson(circ);						
 	destroyList(circ);
 	
 	return 0;
