7fe721dfae2ff1b1327b42a5e2741de502413b53
Dmitriy <glovatskiy.dmitriy@gmail.com>
2017-05-23 08:38:03 +0300

    Delete keyboard.h

--- dk61/Bramory/HW7/keyboard.h
+++
@@ -1,8 +0,0 @@
-#ifndef KEYBOARD_H_INCLUDED
-#define KEYBOARD_H_INCLUDED
-
-void Keyboard(unsigned char key, int x, int y);
-void SKeyboard(int key, int x, int y);
-
-
-#endif // KEYBOARD_H_INCLUDED
