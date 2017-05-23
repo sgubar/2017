c0711d8a54ffe153a9f2b29ccce5223ebd6be2f6
ra7e <artemnakorenko59@gmail.com>
2017-05-23 11:04:24 +0300

    Add files via upload

---
+++ dk61/ra7e/lr2/dk_tool.h
@@ -0,0 +1,23 @@
+#ifndef sort_tools_h
+#define sort_tools_h
+
+#include<stdio.h>
+
+int *createANarray(int Asize);
+
+int fillingANarray(int *ANarray, int Asize);
+
+void writeANarrayInFILE(FILE *file, int *ANarray, int Asize, float spentTime);
+
+float chooseTheKindOfSortAndSpentTime(int *ANarray, int Asize);
+
+void bubbleSort(int anArray[], int aSize);
+
+void selectionSort(int anArray[], int aSize);
+
+void insertionSort(int anArray[], int aSize);
+
+//int scan(int min, int max);/
+
+
+#endif /*sort_tools_h*/
