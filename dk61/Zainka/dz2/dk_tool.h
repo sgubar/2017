b07615ff8c46b1039f23c8dc88f256d9fc4bd81b
Zainka37 <moran.a@list.ru>
2017-05-23 09:53:23 +0300

    Create dk_tool.h

---
+++ dk61/Zainka/dz2/dk_tool.h
@@ -0,0 +1,31 @@
+#ifndef dk_tool_h
+#define dk_tool_h
+
+	typedef	struct KYKI
+	{
+		int x;
+		int y;
+		int z;
+} kordinati;
+
+typedef	struct Piramida
+{
+	kordinati *points[4]; 
+
+} Piramida;
+
+
+typedef struct filepiramida
+{
+	Piramida *Piramida;
+	int size;
+	int current_size;
+}filepiramida;
+
+filepiramida *Createfilepiramida(int aSize);
+ int addPiramida(filepiramida *polochka);
+ float area(Piramida *heops);
+ 
+
+#endif
+ 
