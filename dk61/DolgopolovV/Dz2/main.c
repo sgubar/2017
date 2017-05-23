c33ae7df3836e0f8fa45b02aa13416344e43db42
hogl030 <hogl030@gmail.com>
2017-05-23 06:04:07 +0300

    Update main.c

--- dk61/DolgopolovV/Dz2/main.c
+++ dk61/DolgopolovV/Dz2/main.c
@@ -1,49 +1,18 @@
 #include "dk_tool.h"
 
-
-char ch;
-int size;
-int main(void)
-{	
-while(1)
+int main()
 {
-system("cls"); 
-printf("Select one of the options:\n\n");
-printf("1. Populate list.\n");
-printf("2. Add book.\n");
-printf("3. Remove book.\n");
-printf("4. Show shelf.\n");
-printf("5. Output.\n");
-printf("Enter the number of the selected item:");
-ch = getch();
-if (ch == '1' || ch == '2' || ch == '3' || ch == '4' || ch == '5')
-break;
-}
-switch (ch)
-{
-case '1':
-size = sozd();
-main();
-return 0;
-
-case '2':
-add(size);
-size++;
-main();
-return 0;
-
-case '3':
-del(size);
-size--;
-main();
-return 0;
-
-case '4':
-shov(size, 1);
-main();
-return 0;
-
-case '5':
-return 0;
-}
+	DK_Polka *thePolka = createPolka(20);
+	
+	char theAuthor[] = "Name author";
+	char theNazva[] = "";
+	
+	addBookToPolka(thePolka, "Nikolai Gogol", "Viy");
+	addBookToPolka(thePolka, "Garcia Marquez", "One Hundred Years of Solitude");
+	addBookToPolka(thePolka, "Mikhail Bulgakov", "The Master and Margarita");
+	addBookToPolka(thePolka, "J.R.R.Tolkien", "The Lord of the Rings");
+	
+	printPolka(thePolka);
+		
+	return 0;
 }
