02994c9a660a9d6bc82701d49bc0a21fcb259b17
hogl030 <hogl030@gmail.com>
2017-05-23 06:05:39 +0300

    Update dk_tool.c

--- dk61/DolgopolovV/Dz2/dk_tool.c
+++ dk61/DolgopolovV/Dz2/dk_tool.c
@@ -1,28 +1,68 @@
-#ifndef dk_tool_h
-#define dk_tool_h
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
 
-#include <stdio.h>
 
-typedef struct tagBook
+DK_Polka *createPolka(int aSize)
 {
-	char *Author;
-	char Nazva[100];
-}DK_Book;
+	DK_Polka *thePolka = (DK_Polka *)malloc(sizeof(DK_Polka));
 
-typedef struct tagPolka
-{
-	DK_Book *notes;
-	int size;
-	int current_size;
-}DK_Polka;
-
-DK_Polka *createPolka(int aSize);
+	if (NULL != thePolka)
+	{
+		thePolka->notes = (DK_Book *)malloc(aSize * sizeof(DK_Book));
 	
-int addBookToPolka(DK_Polka *inPolka, char *aAuthor, char *aNazva);
+	
+		thePolka->size = aSize;
+		thePolka->current_size = 0;
+	}
 
-void printBook(DK_Book *aBook);
+	return thePolka;
+}
 
-void printPolka(DK_Polka *inPolka);
+int addBookToPolka(DK_Polka *aPolka, char *aAuthor, char *aNazva)
+{
+	int theResult = 0;
+	
+	if (NULL != aPolka && NULL != aAuthor && NULL != aNazva)
+	{
+		if (aPolka->current_size < aPolka->size)
+		{
+			DK_Book *theBook = &(aPolka->notes[aPolka->current_size]);
+		
+			theBook->Author = (char *)malloc(sizeof(char)*(strlen(aAuthor)+1));
+		
+			strcpy(theBook->Author, aAuthor);
+			strcpy(theBook->Nazva, aNazva);
+		
+			aPolka->current_size ++;
+		}
+		else
+		{
+			theResult = -2;
+		}
+	}
+	else
+	{
+		theResult = -1;
+	}
+	
+	return theResult;
+}
 
+void printBook(DK_Book *aBook)
+{
+	printf("Author: %s\t", aBook->Author);
+	printf("Nazva: %s\n", aBook->Nazva);
+}
 
-#endif
+void printPolka(DK_Polka *inPolka)
+{
+	printf("size: %d\t", inPolka->size);
+	printf("current size: %d\n", inPolka->current_size);
+	
+	for (int i = 0; i < inPolka->current_size; i++)
+	{
+		printf("[%d] - ", i);
+		printBook(&(inPolka->notes[i]));
+	}
+}
