481caff21e744eeef21df343a87151e2282c7d9c
Bogdan Ponomarenko <pb6798@gmail.com>
2017-05-23 20:24:57 +0300

    Create sort.h

---
+++ dk61/bogdanponomarenko/HW4/sort.h
@@ -0,0 +1,12 @@
+
+#ifndef sort_h
+#define sort_h
+
+#include "t.h"
+
+void Bubble_Sort(ListFigures *aFigures);
+void Selection_Sort(ListFigures *aFigures);
+void Insertion_Sort(ListFigures *aFigures);
+
+#endif
+
