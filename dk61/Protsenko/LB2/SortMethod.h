e50958de41a935ebc67d5dced6cb2274f456e8e0
ilyaprotsenko <ilyaprotsenko@ukr.net>
2017-05-23 21:52:54 +0300

    Add files via upload

---
+++ dk61/Protsenko/LB2/SortMethod.h
@@ -0,0 +1,12 @@
+#ifndef SortMethod_h
+#define SortMethod_h
+
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+
+void bubbleSort(char *str, int string_lenght);
+void selectionSort(char *str, int str_lenght);
+void insertionSort(char *str, int string_lenght);
+
+#endif /* SortMethod_h */
