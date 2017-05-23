2c53ed9287e20293c40ab7605e1b50d5c7bc0466
yurbase <yura.kindrat98@gmail.com>
2017-05-23 10:28:35 +0300

    Create dk_tool.h

---
+++ dk61/Kindrat/lb1/dk_tool.h
@@ -0,0 +1,10 @@
+#ifndef test.h
+#define test.h
+
+#include <stdio.h>
+#include <math.h>
+
+int module(int a, int b, int c, int module_result); 
+int sum(int a, int d, int box); 
+int step_c(int c,int box_c);
+#endif 
