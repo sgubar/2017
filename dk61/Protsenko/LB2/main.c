08bb9271efacb118763a611daa1c634c887716e8
ilyaprotsenko <ilyaprotsenko@ukr.net>
2017-05-24 01:32:57 +0300

    Add files via upload

---
+++ dk61/Protsenko/LB2/main.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+#include "SortMethod.h"
+#define MAX 1024
+
+int main(void)
+{
+	FILE *file = fopen("ValueContainer.txt", "r"); //open file to read
+	FILE *file2 = fopen("SortedValues.txt", "w");  //open file to write
+
+	if (fopen == NULL)  //check did the file open
+	{
+		printf("Error...\n");
+		return 0;
+	}
+	char str[MAX];   //creating our string
+	fgets(str, MAX, file); //get values from ValueContainter.txt
+	
+	int string_lenght = strlen(str); //determine lenght of our file
+	
+	printf("Before sorting: %s\n", &str);
+	fprintf(file2, "Before sorting: %s\n", &str);
+	printf("Sorting...\n\n");
+	
+	printf("/*------------------------BUBBLE SORT-------------------------*/");
+	unsigned int BubbleStartTime = clock(); //determine sort time
+	bubbleSort(str, string_lenght); //bubble_sort
+	unsigned int BubbleEndTime = clock();
+	unsigned int BubbleSearchTime = BubbleEndTime - BubbleStartTime;
+	
+	printf("\nAfter sorting: %s\n", &str); //print arr after sort
+	fprintf(file2, "\nAfter sorting %s\n", &str);
+	
+	printf("BubbleSort_Time: %.10f\n\n",(double)(BubbleSearchTime/CLOCKS_PER_SEC)); //print time of execution
+	fprintf(file2,"BubbleSort_Time: %.10f\n",(double)(BubbleSearchTime/CLOCKS_PER_SEC));
+
+
+
+   	printf("/*------------------------SELECTION SORT-------------------------*/");
+	   	if (fseek (file,0,SEEK_SET)==0) //check, has a point moved or isn't
+      	   printf (" ");
+        else
+         printf ("Error\n");
+	   
+    fgets(str, MAX, file);
+    
+	unsigned int SelectionStartTime =  clock();
+	selectionSort(str, string_lenght); //sorting
+	unsigned int SelectionEndTime = clock();
+	unsigned int SelectionSearchTime = SelectionEndTime - SelectionStartTime; 
+	
+	printf("\nAfter sorting: %s\n", &str); //print arr after sort
+	fprintf(file2, "\nAfter sorting %s\n", &str);
+	printf("SelectionSort_Time : %.10f\n\n",(double)(SelectionSearchTime/CLOCKS_PER_SEC));
+	fprintf(file2,"SelectionSort_Time: %.10f\n\n",(double)(SelectionSearchTime/CLOCKS_PER_SEC)); //print time of execution
+
+
+
+   	printf("/*------------------------INSERTION SORT-------------------------*/");	
+	   if (fseek (file,0,SEEK_SET)==0) //check, has a point moved or isn't
+      	   printf (" ");
+        else
+        printf ("Error\n");
+    
+    fgets(str, MAX, file);
+    
+    unsigned int InsertionStartTime =  clock();
+	selectionSort(str, string_lenght); //sorting
+	unsigned int InsertionEndTime = clock();
+	unsigned int InsertionSearchTime = InsertionEndTime - InsertionStartTime;
+	
+	printf("\nAfter sorting: %s", &str); //print arr after sort
+	fprintf(file2, "After sorting %s\n", &str);
+	printf("\nInsertionSort_Time : %.10f\n\n",(double)(InsertionSearchTime/CLOCKS_PER_SEC));
+	fprintf(file2,"InsertionSort_Time: %.10f\n\n",(double)(InsertionSearchTime/CLOCKS_PER_SEC)); //print time of execution
+	printf("\n");
+	
+	
+	printf ("Closing SortedValues: ");
+   		if (fclose (file2) == EOF) //check, is a file2 closed or not
+		  printf ("Error\n");
+   		else
+		  {
+		  printf ("Done\n");
+		  }
+		  
+	printf ("Closing ValueContainer: ");
+   		if (fclose (file) == EOF) //check, is a file closed or not
+		  printf ("Error\n");
+   		else
+		  {
+		  printf ("Done\n");
+		  return 0;
+		  }
+}
