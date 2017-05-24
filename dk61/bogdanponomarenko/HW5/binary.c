caa74f76e666bd7db94913f795145587039400f3
Bogdan Ponomarenko <pb6798@gmail.com>
2017-05-24 08:22:41 +0300

    Update binary.c

--- dk61/bogdanponomarenko/HW5/binary.c
+++ dk61/bogdanponomarenko/HW5/binary.c
@@ -1,1 +1,40 @@
+#include "binary.h"
 
+
+int binarySearch(ListFigures*AFigures, int aValue)
+{
+    int theRightLimit = AFigures->current_size - 1;
+    int theLeftLimit = 0;
+    int theResult ;
+
+
+    while (1)
+    {
+        int middleValue = (theLeftLimit + theRightLimit) / 2;
+
+        if (Square(&AFigures->Rectangles[middleValue]) == aValue)
+        {
+            theResult = middleValue;
+            printf("Searched Pyramid is located on %d position\n", theResult+1);
+            return theResult;
+            break;
+        }
+        else if (theLeftLimit > theRightLimit)
+        {
+            break;
+        }
+        else
+        {
+            if (Square(&AFigures->Rectangles[middleValue]) > aValue)
+            {
+                theRightLimit = middleValue - 1;
+            }
+            else
+            {
+                theLeftLimit = middleValue + 1;
+            }
+        }
+    }
+    return -1;
+
+}
