05f85813a721d4f7d9043efc259f73558971e5d9
Bogdan Ponomarenko <pb6798@gmail.com>
2017-05-24 08:25:00 +0300

    Update t.h

--- dk61/bogdanponomarenko/HW5/t.h
+++ dk61/bogdanponomarenko/HW5/t.h
@@ -1,1 +1,66 @@
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#ifndef t_h
+#define t_h
+
+
+
+
+typedef struct Coordinate
+{
+    int x;
+    int y;
+}  RectanglePoint;
+
+
+typedef struct describe_1_rectangle
+{
+    RectanglePoint * Point;
+
+} Rectangle;
+
+
+typedef struct RectangleList
+{
+    Rectangle *Rectangles;
+    int size;
+    int current_size;
+} ListFigures;
+
+
+ListFigures *CreateListFigure (int aSize);
+
+void DestroyList (ListFigures * aFigures);
+
+int AddRectangle(ListFigures * aFigures);
+
+int printRectangle(Rectangle*theRectangle);
+
+void printRectangleList(ListFigures *aRectangleList);
+
+int Square(Rectangle * Figures);
+
+void swap(Rectangle *var1, Rectangle *var2);
+
+void swap(Rectangle *var1, Rectangle *var2);
+
+void writeList(FILE *aFile, ListFigures *aFigures);
+
+void writeRectangle(FILE *aFile, Rectangle*theRectangle);
+
+void sorting(ListFigures * aFigures);
+
+void searching(ListFigures * aFigures, int value);
+
+
+
+
+#endif
+
+
+
+
+
