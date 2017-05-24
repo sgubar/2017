c10b4553e1aefa96ba2adc5a35b99a9aee0ce7d3
Bogdan Ponomarenko <pb6798@gmail.com>
2017-05-24 08:22:07 +0300

    Update main.c

--- dk61/bogdanponomarenko/HW5/main.c
+++ dk61/bogdanponomarenko/HW5/main.c
@@ -1,1 +1,30 @@
-1
+#include <stdio.h>
+#include "sort.h"
+#include "t.h"
+
+
+int main()
+{
+    int aSize = 0;
+    int sort = 0;
+    int value = 0;
+
+    printf("Number of rectangles :  ");
+    scanf(" %d",&aSize);
+
+    ListFigures * figure = CreateListFigure(aSize);
+
+    for (int i = 0; i < aSize; i++)
+    {
+        AddRectangle(figure);
+    }
+
+    sorting(figure);
+
+    searching(figure,value);
+
+    DestroyList(figure);
+
+    return 0;
+}
+
