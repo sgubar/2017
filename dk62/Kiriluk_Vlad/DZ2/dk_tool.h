b3fa80f217d319f3547d9bc5dddcef3a1498ad30
I-DefenDer <mhuhbjp@yandex.ua>
2017-05-29 22:55:58 +0300

    Create dk_tool.h

---
+++ dk62/Kiriluk_Vlad/DZ2/dk_tool.h
@@ -0,0 +1,27 @@
+#ifndef dk_tool_h
+#define dk_tool_h
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct A_Book
+{
+	char *The_author;
+	char Name_Book[100];
+}BookDescription;
+
+typedef struct The_Shelf
+{
+	BookDescription *Books;
+	int size;
+	int current_size;
+}MyShelf;
+
+MyShelf *createShelf(int aSize);
+
+void printBook(BookDescription *aBook);
+
+void printShelf(MyShelf *inShelf);
+
+#endif
