ee75ce788f4a81fde77e623672e39fb3482ff9a7
Jayawardenepura <kuzhilnyioleg16@gmail.com>
2017-05-29 20:12:22 +0300

    Add files via upload

---
+++ dk62/Oleg_Kuzhilnyi/Labs/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "TestSymmetricList.h"
+#include "TestSymmetricNode.h"
+
+int main(int argc, char* argv[])
+{
+  
+    ListWork(); //Check inputs.txt
+
+    return 0;
+}
+
