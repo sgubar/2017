1db6e7ebbab0d25e2d77ddef5d96e5fc4036bd53
Zainka37 <moran.a@list.ru>
2017-05-29 19:22:31 +0300

    Update sort.c

--- dk61/Zainka/dz4/sort.c
+++ dk61/Zainka/dz4/sort.c
@@ -5,14 +5,14 @@
 void Bubble_Sort_List(filepiramida *polochka)
 {
 	int i, j, result;
-	for (i = 1; i < polochka->current_size; i++)
+	for (i = 1; i < polochka->current_size; i++) // ïðîõîäèìñÿ ïî ìàñèâó
 	{
 		result = 0; 						
-		for (j = 1; j < polochka->current_size - i + 1; j++)
+		for (j = 1; j < polochka->current_size - i + 1; j++)  // íàõ íàéá³ëüøèé
 		{
-			if (area(&polochka->Piramida[j]) > area(&polochka->Piramida[j - 1]))
+			if (area(&polochka->Piramida[j]) > area(&polochka->Piramida[j - 1])) // ïîð³âíþåìî 1 ñ ïðåä³äóùèì 
 			{
-				swap(&polochka->Piramida[j], &polochka->Piramida[j - 1]);
+				swap(&polochka->Piramida[j], &polochka->Piramida[j - 1]); // çì³íþåìî ¿õ 
 				result++; 					
 			}
 		}
