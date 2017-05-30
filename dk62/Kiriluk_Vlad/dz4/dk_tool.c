20eb0536ba923e14178d8077b4256eff6ee8247e
I-DefenDer <mhuhbjp@yandex.ua>
2017-05-30 23:04:07 +0300

    Create dk_tool.c

---
+++ dk62/Kiriluk_Vlad/dz4/dk_tool.c
@@ -0,0 +1,106 @@
+#include "dk_tool.h"
+#include <stdlib.h>
+#include <string.h>
+
+
+
+MyShelf *createShelf(int aSize)
+{
+	MyShelf *theShelf = (MyShelf *)malloc(sizeof(MyShelf));
+
+	if (NULL != theShelf)
+	{
+		theShelf->Books = (BookDescription *)malloc(aSize * sizeof(BookDescription));
+		theShelf->aSize = aSize;
+		theShelf->current_size = 0;
+	}
+
+	return theShelf;
+}
+
+int addBookToMyShelf(MyShelf *aShelf, char *aAuthor, char *aNazva)
+{
+int theResult = 0;
+
+	if (NULL != aShelf && NULL != aAuthor && NULL != aNazva)
+	{
+		if (aShelf->current_size < aShelf->aSize)
+		{
+			BookDescription *theBook = &(aShelf->Books[aShelf->current_size]);
+
+			theBook->The_author = (char *)malloc(sizeof(char)*(strlen(aAuthor)+1));
+
+			strcpy(theBook->The_author, aAuthor);
+			strcpy(theBook->Name_Book, aNazva);
+
+			aShelf->current_size ++;
+		}
+	}
+	else
+	{
+		theResult = -1;
+	}
+
+	return theResult;
+}
+
+void printBook(BookDescription *aBook)
+{
+	printf("Author: %s\t", aBook->The_author);
+	printf("Name book: %s\n", aBook->Name_Book);
+}
+
+void printShelf(MyShelf *inShelf)
+{
+	printf("size: %d\t", inShelf->aSize);
+	printf("current size: %d\n", inShelf->current_size);
+
+	for (int i = 0; i < inShelf->current_size; i++)
+	{
+		printf("[%d] - ", i);
+		printBook(&(inShelf->Books[i]));
+	}
+}
+
+void BubbleSortShelf(MyShelf *theShelf, int aSize)
+{
+	int theOut;
+
+	for (theOut; theOut > 1; theOut --)
+	{
+		int theIn = 0;
+
+		for (theIn; theIn < theOut; theIn ++)
+		{
+			if (theShelf->Books[theIn].Name_Book > theShelf->Books[theIn+ 1].Name_Book )
+			{
+				BookDescription *timeBook = theShelf->Books[theIn].Name_Book;
+				theShelf->Books[theIn].Name_Book = theShelf->Books[theIn + 1].Name_Book;
+                theShelf->Books[theIn + 1].Name_Book = timeBook;
+			}
+		}
+	}
+}
+
+
+void destroyShelf(MyShelf *aShelf)
+{
+	if (NULL != aShelf)
+	{
+		if(NULL != aShelf->Books)
+		{
+			for (int i = 0; i < aShelf->current_size; i++)
+			{
+				BookDescription theBook = aShelf->Books[i];
+				if (NULL != theBook.The_author)
+				{
+					free(theBook.The_author);
+				}
+			}
+
+			free(aShelf->Books);
+		}
+
+		free(aShelf);
+	}
+}
