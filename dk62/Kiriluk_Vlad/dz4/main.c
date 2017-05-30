885868dd19ba90bb7be33b4ba90e9340c74074af
I-DefenDer <mhuhbjp@yandex.ua>
2017-05-30 22:52:59 +0300

    Create main.c

---
+++ dk62/Kiriluk_Vlad/dz4/main.c
@@ -0,0 +1,19 @@
+#include "dk_tool.h"
+
+int main()
+{
+	MyShelf *theShelf = createShelf(20);
+
+
+	addBookToMyShelf(theShelf, "J. K. Rowling", "Harry Potter");
+	addBookToMyShelf(theShelf, "Leo Tolstoy", "Anna Karenina");
+	addBookToMyShelf(theShelf, "Fodor Dostoyevsky", "Crime and Punishment");
+	addBookToMyShelf(theShelf, "Stephen King", "It");
+	addBookToMyShelf(theShelf, "Dante Alighieri", "Divine Comedy");
+	addBookToMyShelf(theShelf, "J.R.R.Tolkien", "The Lord of the Rings");
+	addBookToMyShelf(theShelf, "Oscar Wilde", "The Picture of Dorian Gray");
+
+	printShelf(theShelf);
+	printShelfJson(theShelf);
+	destroyShelf(theShelf);
+}
