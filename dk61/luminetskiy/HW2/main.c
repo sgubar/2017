d8c2e3cf792931a0c59c29d198946e354975c82e
romamaka <lumineckiyy447@gmail.com>
2017-05-30 10:03:37 +0300

    Update main.c

--- dk61/luminetskiy/HW2/main.c
+++ dk61/luminetskiy/HW2/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "circling.h"
+#include "dk_tool.h"
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
