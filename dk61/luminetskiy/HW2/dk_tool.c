0cd03b3bb2713952e89ee65457eaafb3f7a49d83
romamaka <lumineckiyy447@gmail.com>
2017-05-30 09:59:45 +0300

    Update dk_tool.c

--- dk61/luminetskiy/HW2/dk_tool.c
+++ dk61/luminetskiy/HW2/dk_tool.c
@@ -1,11 +1,7 @@
+#include "circling.h"
 #include <stdio.h>
 //#include <stdbool.h>
-#include "dk_tool.h"
-circle * CreateList(int size)
-{
-	circle *newlist(circle *)malloc(sizeof(circle)*size);
-	return newlist;
-}
+
 void NewCircle(circle *ptr)
 {
     printf("PLease write the coordinates of the center(2 points)\nX:\n");
@@ -18,15 +14,16 @@ void NewCircle(circle *ptr)
         printf("Error! Radius is less than 0!\n");
         scanf("%d",&ptr->rad);
     }
-    
+
 }
-long double square(circle s)
+
+double Squareof(circle s)
 {
-	return Pi*circle.rad * circle.rad;
+    return Pi * s.rad * s.rad ;
 }
-void ShowTheFigures(square ptr[],int n) 
+void ShowTheFigures(circle ptr[],int n)
 {
-    for (int k = 0; k < n; k++) 
+    for (int k = 0; k < n; k++)
 	{
         printf("Circle number %i with center(%i,%i), radius %i, and square %i\n",k+1,ptr[k].x,ptr[k].y,ptr[k].rad,ptr[k].square);
     }
