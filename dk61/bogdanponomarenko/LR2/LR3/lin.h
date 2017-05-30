a721893f1b479faf12554a3efb36e546553b9f83
Bogdan Ponomarenko <pb6798@gmail.com>
2017-05-30 10:48:59 +0300

    Add files via upload

---
+++ dk61/bogdanponomarenko/LR2/LR3/lin.h
@@ -0,0 +1,11 @@
+#ifndef linear_h
+#define linear_h
+
+#include <stdio.h>
+
+void linear(int anArray[], int Asize);
+
+#endif 
+
+
+
---
+++ dk61/bogdanponomarenko/LR2/LR3/t.h
@@ -0,0 +1,29 @@
+#ifndef t_h
+#define t_h
+
+#include <stdio.h>
+
+int *createArray(int Asize);
+
+int fillingArray(int *AnArray, int Asize);
+
+void WriteToFile(FILE *file, int *AnArray, int Asize, float Time);
+
+float Sort(int *AnArray, int Asize);
+
+void bubbleSort(int anArray[], int aSize);
+
+void selectionSort(int anArray[], int aSize);
+
+void insertionSort(int anArray[], int aSize);
+
+int scan(int min, int max);
+
+void Show(int *AnArray, int Asize, float Time);
+
+
+
+#endif 
+
+
+
