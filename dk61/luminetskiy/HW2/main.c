b8f7650a3299e049106a253c2119ba768262f7c6
romamaka <lumineckiyy447@gmail.com>
2017-05-30 10:00:23 +0300

    Update main.c

--- dk61/luminetskiy/HW2/main.c
+++ dk61/luminetskiy/HW2/main.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "circling.h"
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
 	int size;
 	printf("Write number of figures: \n");
 	scanf("%i",&size);
-	circle *mass =(circle *)malloc(sizeof(circle)*size);
-	
+	circle *mass =(circle *) malloc(sizeof(circle)*size);
+    for (int i = 0; i < size; ++i) {
+        NewCircle(&mass[i]);
+        mass[i].square = Squareof(mass[i]);
+    }
+    ShowTheFigures(mass,size);
 	return 0;
 }
