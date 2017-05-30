72681d7d2cfa4f1da97ada66c4baa45e1339533c
Dmitriy <glovatskiy.dmitriy@gmail.com>
2017-05-30 11:01:09 +0300

    Add files via upload

---
+++ dk61/GorunovIlya/Lab4/TLFloat.h
@@ -0,0 +1,54 @@
+#ifndef TLIntList_h
+#define TLIntList_h
+
+#include <stdio.h>
+
+
+typedef struct __tagFloatNode FloatNode;
+
+struct __tagFloatNode
+{
+	float value;
+	FloatNode *prev;
+	FloatNode *next;
+};
+
+
+typedef struct __tagFloatList
+{
+	FloatNode *head;
+	FloatNode *tail;
+	int count;
+}FloatList;
+
+
+FloatList *TLCreateFloatList();
+void TLDestroyFloatList(FloatList *aList);
+
+
+int TLAddFloatValueToList(FloatList *aList, float anFloatValue);
+
+
+int TLInsertFloatValueAtIndex(FloatList *aList, float anFloatValue, int anIndex);
+
+
+float TLFloatValueAtIndex(FloatList *aList, int anIndex);
+
+FloatNode *FindFloatValueAtIndex(FloatList *aList, int anIndex);
+
+
+void TLPrintList(FloatList *aList);
+
+int TLDestroyFloatValueAtIndex(FloatList *aList, int anIndex);
+//void shellSort(FloatList *aList);
+//void swap(FloatList *aList, int firstIndex, int secondIndex);
+
+
+
+//lesser than average
+int deleteFirstFloatElement(FloatList *aList, int aValue);
+	
+float FindAverageInList(FloatList *aList);
+
+
+#endif
