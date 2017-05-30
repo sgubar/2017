8d9a12a70a4f925ecbf1cb069462df5e223a1ba7
I-DefenDer <mhuhbjp@yandex.ua>
2017-05-30 23:03:08 +0300

    Create main.c

---
+++ dk62/Kiriluk_Vlad/dz4/main.c
@@ -0,0 +1,21 @@
+#include "dk_tool.h"
+
+int main()
+{
+	MyShelf *theShelf = createShelf(20);
+
+
+	addBookToMyShelf(theShelf, "J. K. Rowling", "Harry Potter");
+	addBookToMyShelf(theShelf, "Leo Tolstoy", "Anna Karenina");
+	addBookToMyShelf(theShelf, "Fodor Dostoyevsky", "Crime and Punishment");
+	addBookToMyShelf(theShelf, "Stephen King", "It");
+	addBookToMyShelf(theShelf, "Dante Alighieri", "Divine Comedy");
+	addBookToMyShelf(theShelf, "J.R.R.Tolkien", "The Lord of the Rings");
+	addBookToMyShelf(theShelf, "Oscar Wilde", "The Picture of Dorian Gray");
+
+  printShelf(theShelf);
+	printf ("After sorting \n");
+	void BubbleSortShelf(theShelf, aSize);
+	printShelf(theShelf);
+	destroyShelf(theShelf);
+}
