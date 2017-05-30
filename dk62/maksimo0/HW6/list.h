39da9108d691161f7c074b060857786ab7a483d0
maksimo0 <mkdjsl@gmail.com>
2017-05-31 00:31:45 +0300

    Add files via upload

---
+++ dk62/maksimo0/HW6/list.h
@@ -0,0 +1,46 @@
+//
+// Created by Max on 29.05.2017.
+//
+
+#ifndef list_h
+#define list_h
+
+#include<stdio.h>
+
+typedef struct __tagIntNode IntNode;
+
+struct __tagIntNode
+{
+    int value;
+    IntNode *prev;
+    IntNode *next;
+};
+
+typedef struct __tagIntList
+{
+    IntNode *head;
+    IntNode *tail;
+    int count;
+}IntList;
+
+IntList *CreateIntList();
+void DestroyIntList(IntList *aList);
+
+int AddIntValueToList(IntList *aList, int anIntValue);
+
+IntNode *IntValueAtIndex(IntList *aList, int anIndex);
+
+void PrintList(IntList *aList);
+
+int InsertIntValueAtIndex(IntList *aList, int anIntValue, int anIndex);
+
+void DeleteIntValueAtIndex(IntList *aList, int anIndex);
+
+void quickSort(IntList *aList, int aLeftIndex, int aRightIndex);
+void m_manualSort(IntList *aList, int aLeftIndex, int aRightIndex);
+int mediana(IntList *aList, int aLeftIndex, int aRightIndex);
+int m_partitionIt(IntList *aList, int aLeftIndex, int aRightIndex, int aPivot);
+
+void swap(IntList *aList, int aLeftIndex, int aRightIndex);
+
+#endif
