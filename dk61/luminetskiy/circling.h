79a9748fb12034d37a161d8e3118eff1c497e75c
romamaka <lumineckiyy447@gmail.com>
2017-05-31 20:30:24 +0300

    Add files via upload

---
+++ dk61/luminetskiy/circling.h
@@ -0,0 +1,15 @@
+#ifndef ROMA_CIRCLING_H
+#define ROMA_CIRCLING_H
+#define Pi 3.14159
+
+typedef struct {
+    int x;
+    int y;
+    int rad;
+    double square;
+} circle;
+void NewCircle(circle *ptr);//adding fig to array
+double Square(circle s);
+void ShowTheFigures(circle ptr[],int n);
+
+#endif //ROMA_CIRCLING_H
---
+++ dk61/luminetskiy/sort.h
@@ -0,0 +1,8 @@
+#ifndef ROMA_CIRCLING_H
+#define ROMA_CIRCLING_H
+#include "circling.h"
+void swap(circle *a, circle *s);
+void bubble(circle array[],int size);
+void shell(circle array[],int size);
+int linear(circle array[],int size, double d);
+#endif //ROMA_CIRCLING_H
