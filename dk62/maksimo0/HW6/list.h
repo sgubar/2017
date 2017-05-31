e5774c7604baf143f9c0334244fbad8b2b64f71d
maksimo0 <mkdjsl@gmail.com>
2017-05-31 09:11:32 +0300

    Update list.h

--- dk62/maksimo0/HW6/list.h
+++ dk62/maksimo0/HW6/list.h
@@ -1,78 +1,40 @@
 //
-
 // Created by Max on 29.05.2017.
-
 //
 
-
-
 #ifndef list_h
-
 #define list_h
-
-
-
 #include<stdio.h>
-
-
 
 typedef struct __tagIntNode IntNode;
 
-
-
 struct __tagIntNode
-
 {
-
     int value;
-
     IntNode *prev;
-
     IntNode *next;
-
 };
 
-
-
 typedef struct __tagIntList
-
 {
-
     IntNode *head;
-
     IntNode *tail;
-
     int count;
-
 }IntList;
-
-
 
 IntList *CreateIntList();
 
 void DestroyIntList(IntList *aList);
 
-
-
 int AddIntValueToList(IntList *aList, int anIntValue);
-
-
 
 IntNode *IntValueAtIndex(IntList *aList, int anIndex);
 
-
-
 void PrintList(IntList *aList);
-
-
 
 int InsertIntValueAtIndex(IntList *aList, int anIntValue, int anIndex);
 
-
-
 void DeleteIntValueAtIndex(IntList *aList, int anIndex);
-
-
 
 void quickSort(IntList *aList, int aLeftIndex, int aRightIndex);
 
@@ -82,10 +44,6 @@ int mediana(IntList *aList, int aLeftIndex, int aRightIndex);
 
 int m_partitionIt(IntList *aList, int aLeftIndex, int aRightIndex, int aPivot);
 
-
-
 void swap(IntList *aList, int aLeftIndex, int aRightIndex);
-
-
 
 #endif
