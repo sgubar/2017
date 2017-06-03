a48830784ff919816087859a01088731433c5bd6
Zainka37 <moran.a@list.ru>
2017-06-03 18:20:48 +0300

    Create dk_tool.c

---
+++ dk61/Zainka/lab3/dk_tool.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <memory.h>
+#include <ctype.h>
+#include <time.h>
+#include "dk_tool.h"
+#include "shell_sort.h"
+
+int binary_search(char key, int aSize, char *Sortmassiv)
+{
+    
+    
+	int left = 0, right = aSize-1, middle;
+
+    while (left <= right )
+    {
+    	middle =   (left+right)/2;
+     if (Sortmassiv[middle] == key)
+	  return middle; 
+     else 
+	 if (Sortmassiv[middle] > key)
+            right = middle-1;		
+        else
+            left = middle+1;			
+
+}
+   return -1;
+           
+
+}
