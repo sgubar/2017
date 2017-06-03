23b1ccddb103286d5bf3c95ca5e1ee7561d82677
Zainka37 <moran.a@list.ru>
2017-06-03 18:21:10 +0300

    Create dk_tool.h

---
+++ dk61/Zainka/lab3/dk_tool.h
@@ -0,0 +1,10 @@
+#ifndef dk_tool_h
+#define dk_tool_h
+#include "shell_sort.h"
+
+
+void swap (char *var1, char *var2);
+int binary_search(char key, int Size, char *Sortmassiv);
+
+//int poisck(char*Sortmassiv);
+#endif
