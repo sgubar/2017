76656bc1486acd213c62add1ec8a24bb5ce30444
Zainka37 <moran.a@list.ru>
2017-06-03 22:37:23 +0300

    Create sort.h

---
+++ dk61/Zainka/laba2/sort.h
@@ -0,0 +1,10 @@
+#ifndef sort_h
+#define sort_h
+
+#include "dk_tool.h"
+char *bubble_sort( int Size, char *neotsortmassiv );
+void swap (char *var1, char *var2);
+char *selection_sort( int Size, char *neotsortmassiv );
+char *insertion_sort(int Size , char *neotsortmassiv);
+//char *selectionSort(char *neotsortmassiv);
+#endif
