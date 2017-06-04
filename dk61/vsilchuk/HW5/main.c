e21e7ddfa1e2eaeff3838fc3503f8ed3a27c7fcc
Vitaliy Silchuk <nubb99@gmail.com>
2017-06-04 17:48:46 +0300

    Update main.c

--- dk61/vsilchuk/HW5/main.c
+++ dk61/vsilchuk/HW5/main.c
@@ -24,25 +24,25 @@ int main()
 			addPyramid(aPyramidsList);
 	}	
 	
-	// выводим наши пирамиды и площади, пишем в файл
+	
 	printPyramidsList(aPyramidsList);
 	FilePrintPyramidsList(ptrFile, aPyramidsList);
 
 	printSquare(aPyramidsList);
 	FilePrintSquare(ptrFile, aPyramidsList);
 	
-	//сортируем
+	
 	sortType(ptrFile, aPyramidsList);
 	
-	//выводим на экран площади после сортировки
+	
 	printf("\n\nAfter sorting: \n");
 	printSquare(aPyramidsList);
 	
-	//пишем в файл площади после сортировки
+	
 	fprintf(ptrFile, " \n\n\nAfter sorting: \n");
 	FilePrintSquare(ptrFile, aPyramidsList);
 
-	//ищем нужное значение площади
+	
 	searchFunction(aPyramidsList);
 	
 	destroyAPyramidsList(aPyramidsList);
