76c8c2fdd69ef23a32f17d42945cca8ef529f124
LIKIL <likil_thrasher@ukr.net>
2017-06-06 00:47:11 +0300

    Create dk_tool.h

---
+++ dk61/Bogolyubov/LW2/dk_tool.h
@@ -0,0 +1,12 @@
+
+#ifndef dk_tool_h
+#define dk_tool_h
+
+#include <stdio.h>
+#include <string.h>
+
+void bubbleSort(char *str, int lenght);
+void selectionSort(char *str, int str_lenght);
+void insertionSort(char *str, int lenght);
+
+#endif
