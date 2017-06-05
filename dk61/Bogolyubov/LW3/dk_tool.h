42bb330d9b963091912c671fb6382b50ac450f55
LIKIL <likil_thrasher@ukr.net>
2017-06-06 01:51:34 +0300

    Create dk_tool.h

---
+++ dk61/Bogolyubov/LW3/dk_tool.h
@@ -0,0 +1,9 @@
+#ifndef dk_tool_h
+#include <stdio.h>
+#include <string.h>
+#define dk_tool_h
+
+void ShellSort(char *str, int lenght);
+int LineSearch(FILE* file2, char marker, char *str, int lenght);
+
+#endif
