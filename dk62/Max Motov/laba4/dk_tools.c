a48becf912481a11e3341f06327b9eaa9b3ce014
Madranko <motov.maxim@gmail.com>
2017-06-05 23:35:00 +0300

    Create dk_tools.c

---
+++ dk62/Max Motov/laba4/dk_tools.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dk_tools.h"
+#pragma warning (disable: 4996)
+
+
+void AddNode(Tree **myTree, int value)
+{
+	Tree *temp = NULL;
+	if (!(*myTree))
+	{
+		temp = (Tree *)malloc(sizeof(Tree));
+		temp->left = temp->right = NULL;
+		temp->data = value;
+		*myTree = temp;
+		return;
+	}
+
+
+	if (value < (*myTree)->data)
+	{
+		AddNode(&(*myTree)->left, value);
+	}
+	else if (value >(*myTree)->data)
+	{
+		AddNode(&(*myTree)->right, value);
+	}
+}
+
+void PrintElements(Tree *myNode)
+{
+	if (myNode) {
+		PrintElements(myNode->left);
+		printf("%d ", myNode->data);
+		PrintElements(myNode->right);
+	}
+
+}
+
+void DeleteElements(Tree *myTree) {
+
+	if (myTree) {
+		DeleteElements(myTree->left);
+		DeleteElements(myTree->right);
+		free(myTree);
+		printf("Element deleted\n");
+	}
+}
+
+Tree *Search(Tree **myTree, int value) {
+
+	if (!(*myTree)) {
+		return NULL;
+	}
+
+	if (value == (*myTree)->data) {
+		return *myTree;
+	}
+	else if (value < (*myTree)->data) {
+		Search(&((*myTree)->left), value);
+	}
+	else if (value >(*myTree)->data) {
+		Search(&((*myTree)->right), value);
+	}
+}
+
