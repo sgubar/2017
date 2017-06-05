6a746862173e64d9731c0dbd2840f9978367467c
hogl030 <hogl030@gmail.com>
2017-06-06 01:51:49 +0300

    Update main.c

--- dk61/DolgopolovV/Dz4/main.c
+++ dk61/DolgopolovV/Dz4/main.c
@@ -4,16 +4,16 @@ int main()
 {
 	DK_Shelf *theShelf = createShelf(20);
 	
+	addBookToShelf(theShelf, "J.R.R.Tolkien", "The Lord of the Rings");
 	addBookToShelf(theShelf, "Nikolai Gogol", "Viy");
 	addBookToShelf(theShelf, "Garcia Marquez", "One Hundred Years of Solitude");
 	addBookToShelf(theShelf, "Mikhail Bulgakov", "The Master and Margarita");
-	addBookToShelf(theShelf, "J.R.R.Tolkien", "The Lord of the Rings");
+	
 	
 	printShelf(theShelf);
 	
-	sortShelf(DK_Shelf *inShelf);
-
-	printf ("After sorting ");
-	printShelf(theShelf);
+	sortShelf(theShelf);
 	
+	printf ("After sorting\n");
+	printShelf(theShelf);	
 }
