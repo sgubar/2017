58a2e09cb424f8076bc87fa446c0de0e299acf89
yurbase <yura.kindrat98@gmail.com>
2017-06-06 04:47:27 +0300

    Create dk_tool.h

---
+++ dk61/Kindrat/lb2/dk_tool.h
@@ -0,0 +1,12 @@
+#ifndef dk_tool.h
+#define dk_tool.h
+
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+
+void bubbleSort(char *str, int lenght);
+void selectionSort(char *str, int lenght);
+void insertionSort(char *str, int lenght);
+
+#endif 
