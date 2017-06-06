4d56166538df97cfcc600ead2556a8e64ea795d0
Kufaeva2103 <sasha99549@gmail.com>
2017-06-06 12:28:33 +0300

    Update main.c

--- dk62/Kufaeva_Sasha/Homework/lab4/main.c
+++ dk62/Kufaeva_Sasha/Homework/lab4/main.c
@@ -5,22 +5,22 @@
 
 int main()
 {
-	//создаём пустой список
+	
 	List *theList = createList();
 	
-	//если создало список
+	
 	if(theList != NULL)
 	{
-		//заполняем его элементами
+		
 		fillElements(theList);
 		
-		//выводим список в нормальном порядке
+		
 		printList(theList);
 		
-		//выводим список как в задании
+		
 		rePrintList(theList);
 		
-		//удаляем список
+		
 		deleteList(theList);
 		
 		return 0;
