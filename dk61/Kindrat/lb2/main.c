cf6d86e7f307d0a462984ad53f29f9b62a781a50
yurbase <yura.kindrat98@gmail.com>
2017-06-06 04:46:00 +0300

    Update main.c

--- dk61/Kindrat/lb2/main.c
+++ dk61/Kindrat/lb2/main.c
@@ -1,1 +1,82 @@
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+#include "dk_tool.h"
+#define MAX 1024
 
+int main(void)
+{
+	FILE *file = fopen("neotsort.txt", "r");
+	FILE *file2 = fopen("otsort.txt", "w");  
+
+	if (fopen == NULL) 
+	{
+		printf("Error...\n");
+		return 0;
+	}
+	char str[MAX];   
+	fgets(str, MAX, file); 
+	
+	int lenght = strlen(str); 
+	
+	printf("Before sorting: %s\n", &str);
+	fprintf(file2, "Before sorting: %s\n", &str);
+	printf("Sorting...\n\n");
+	
+	printf("bubble sort");
+
+	bubbleSort(str, lenght); 
+	
+	printf("\nAfter sorting: %s\n", &str);
+	fprintf(file2, "\nAfter sorting %s\n", &str);
+	
+
+
+
+   	printf("selection sort");
+	   	if (fseek (file,0,SEEK_SET)==0) 
+      	   printf (" ");
+        else
+         printf ("Error\n");
+	   
+    fgets(str, MAX, file);
+    
+	selectionSort(str, lenght); 
+	
+	printf("\nAfter sorting: %s\n", &str); 
+	fprintf(file2, "\nAfter sorting %s\n", &str);
+
+
+
+   	printf("insertion sort");	
+	   if (fseek (file,0,SEEK_SET)==0) 
+      	   printf (" ");
+        else
+        printf ("Error\n");
+    
+    fgets(str, MAX, file);
+    
+	selectionSort(str, lenght); 
+	
+	printf("\nAfter sorting: %s", &str); 
+	fprintf(file2, "After sorting %s\n", &str);
+	printf("\n");
+	
+	
+	printf ("Closing neotsort: ");
+   		if (fclose (file2) == EOF) 
+		  printf ("Error\n");
+   		else
+		  {
+		  printf ("Done\n");
+		  }
+		  
+	printf ("Closing otsort: ");
+   		if (fclose (file) == EOF) 
+		  printf ("Error\n");
+   		else
+		  {
+		  printf ("Done\n");
+		  return 0;
+		  }
+}
