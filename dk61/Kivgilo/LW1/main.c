55287315948e0a082ba620e188f11b0ffa66b1dc
Moskito1902 <kivgilov1902@gmail.com>
2017-06-07 00:25:53 +0300

    Create main.c

---
+++ dk61/Kivgilo/LW1/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "tool.h" 
+
+int main()
+{
+    float X=0; 
+    float Y=0; 
+    float Q=0; 
+    float A,C; 
+    float B; 
+    float D=0; 
+    float sum; 
+
+    printf("A=");
+    scanf("%f",&A); 
+    printf("\n");
+
+    printf("B=");
+    scanf("%f",&B); 
+    printf("\n");
+
+    printf("C=");
+    scanf("%f",&C); 
+    printf("\n");
+
+
+vtoroy_faktorial( A, C,B) ;   
+   
+}
