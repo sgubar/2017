619286dd92ddc4ebfcd41d83010f3569cd2f759f
Olesya <olesya.shut@mail.ru>
2017-06-06 09:54:15 +0300

    Update sort.h

--- dk62/OlesyaShut/laba3/sort.h
+++ dk62/OlesyaShut/laba3/sort.h
@@ -7,7 +7,7 @@ void bubblesort(int *array, int aSize);
 void selectionsort(int *array, int aSize);
 void insertionsort(int *array, int aSize);
 void shellsort(int *array, int aSize);
-void quicksort(int *array, int first, int last);
+
 
 int lineSearch(int val, int aSize, int *array);
 
