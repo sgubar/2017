24b99f9d463cee8eaa216237c266823efbaab84d
Moskito1902 <kivgilov1902@gmail.com>
2017-06-06 07:10:16 +0300

    Create main.c

---
+++ dk61/Kivgilo/LW2/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "tool.h"
+#include <locale.h>
+
+
+int main(void)
+{  
+	setlocale(LC_ALL, "Rus"); 
+	FILE *aFile = fopen("data.txt","r");
+    int length = LengthFile(aFile); //length of file;
+	
+	char *MasFile = StrFile(aFile, length); //create mass of symbol for selection sorting;
+	selectionSort(length, MasFile);
+        
+	char *MasFile1 = StrFile(aFile, length); //create mass of symbol for bubble sorting;
+	bubbleSort(length, MasFile1);
+	
+	char *MasFile2 = StrFile(aFile, length); //create mass of symbol for insertion sorting;
+    insertionSort(length, MasFile2);
+    
+    
+    PrintFile(length, MasFile2); //print result sorting in file;
+	
+	free(MasFile); 
+	free(MasFile1);
+	free(MasFile2);
+	fclose(aFile);
+	
+    return 0;
+}
