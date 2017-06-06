115423ec3d1fa449696e9147e2a38ef9cd26d13c
Stanis209 <dante123321123@gmail.com>
2017-06-06 07:14:06 +0300

    Create tool.h

---
+++ dk61/Havulyak/Laba 2/tool.h
@@ -0,0 +1,8 @@
+#ifndef dk_tools
+#define dk_tools
+
+void SelectionSort(char str[], int counter);
+void BubbleSort(char str[], int counter);
+void InsertionSort(char str[], int counter);
+
+#endif // dk_tools
