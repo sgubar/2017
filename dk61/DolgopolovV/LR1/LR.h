b11d3872675f1c9fd3cbf86db018191c613e12f1
hogl030 <hogl030@gmail.com>
2017-06-06 05:13:22 +0300

    Create LR.h

---
+++ dk61/DolgopolovV/LR1/LR.h
@@ -0,0 +1,13 @@
+#ifndef _LR.h
+#define _LR.h
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void Factorial(int A, int *factorial);
+
+void Modul(int A, int B, int *ModuledZnamenatel, int *Znamenatel);
+
+void Result(int factorial, int *Znamenatel, int *ModuledZnamenatel, float *result);
+
+#endif 
