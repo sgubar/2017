1ef5b2700f872788b672bf53d931c95d89619f0d
yurbase <yura.kindrat98@gmail.com>
2017-06-06 03:15:14 +0300

    Create main.c

---
+++ dk61/Kindrat/dz4/main.c
@@ -0,0 +1,20 @@
+include "dk_tool.h"
+
+int main()
+{
+	Rack *thePolka = createPolka(20);
+	
+	addBookToPolka(thePolka, "Rei Bredberi", "451 po Farengeitu");
+	addBookToPolka(thePolka, "Rei Bredberi", "Vino iz Oduvanchikov");
+	addBookToPolka(thePolka, "Antuan de Sent-Ekzuperi", "Malenki princ");
+	addBookToPolka(thePolka, "Lev Tolstoi", "Anna Karenina");
+	addBookToPolka(thePolka, "Fedor Mihalovich Dostoevski", "Prestuplenie i nakazanie");
+	
+	printPolka(thePolka);
+	printPolkaJson(thePolka);
+	
+	BsortPolka(thePolka);
+	printf ("After sorting\n");
+	printPolka(thePolka);	
+	
+}
