4a1f339ec774c03728aa3aae1419ceaeccea5d56
Stanis209 <dante123321123@gmail.com>
2017-06-06 22:10:06 +0300

    Create main.c

---
+++ dk61/Havulyak/Laba 5/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "tool.h"
+
+int main(int argc, char *argv[]) 
+{
+	FloatTree *Tree = createFloatTree();
+	
+	floatValueToTree(Tree, 11.1);
+	floatValueToTree(Tree, 22.2);
+	floatValueToTree(Tree, 33.3);
+	floatValueToTree(Tree, 44.4);
+	floatValueToTree(Tree, 55.5);
+	floatValueToTree(Tree, 66.6);
+	floatValueToTree(Tree, 77.7);
+	floatValueToTree(Tree, 88.8);
+	
+	int counter = counterNodes(Tree);
+	printf("The number of nodes in  the tree :%d\n", counter);
+	
+	printTree(Tree);
+	printf("\n");
+	printf("We want to find 77.7\n");
+	FloatNode* find = findNodeWithValue(Tree, 77.7);
+	if (find != NULL)
+		printf("That node is in tree.\n");
+	else
+		printf("That node isn't found.\n");
+		
+	FloatNode *aNode = Tree->root;
+	deleteNodeWithValue(Tree,aNode, 66.6);
+	printf("We delete 6.6\n");
+		printf("We want to find 66.6\n");
+	 find = findNodeWithValue(Tree, 66.6);
+	if (find != NULL)
+		printf("That node is in tree\n");
+	else
+		printf("That node isn't found\n");
+	counter=0;
+	counter = counterNodes(Tree);
+	printf("\n\n");
+	printf("The number of nodes in the tree :%d\n", counter);
+	
+	printTree(Tree);
+	
+	destroyFloatTree(Tree);
+	return 0;
+}
