7269c95013d774a4febcafec3c64fb3b3d715271
hogl030 <hogl030@gmail.com>
2017-06-07 03:53:13 +0300

    Create dk_tool.h

---
+++ dk61/DolgopolovV/LR2/dk_tool.h
@@ -0,0 +1,12 @@
+#ifndef dk_tool_h
+#define dk_tool_h
+
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+
+void bubbleSort(char *str, int string_lenght);
+void selectionSort(char *str, int string_lenght);
+void insertionSort(char *str, int string_lenght);
+
+#endif 
