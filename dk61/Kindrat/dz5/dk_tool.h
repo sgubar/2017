b0a923a81d46205cbcbef6428cb3823e7873b425
yurbase <yura.kindrat98@gmail.com>
2017-06-07 04:37:57 +0300

    Create dk_tool.h

---
+++ dk61/Kindrat/dz5/dk_tool.h
@@ -0,0 +1,33 @@
+#ifndef dk_tool_h
+#define dk_tool_h
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct Kniga
+{
+	char *Author;
+	char Name[100];
+}Book;
+
+typedef struct Polka
+{
+	Book *Books;
+	int size;
+	int current_size;
+}Rack;
+
+Rack *createPolka(int aSize);
+	
+int addBookToPolka(Rack *aPolka, char *aAuthor, char *aName);
+
+void printBook(Book *aBook);
+
+void printPolka(Rack *inPolka);
+
+void shellSort(Rack *aPolka );
+
+int binarySearch(Rack *aPolka, char aValue, int size);
+
+#endif
