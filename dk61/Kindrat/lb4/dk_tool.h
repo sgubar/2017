f626f079f6d7a0cebc152c4d2d746f55d4e4f917
yurbase <yura.kindrat98@gmail.com>
2017-06-25 21:45:09 +0300

    Create dk_tool.h

---
+++ dk61/Kindrat/lb4/dk_tool.h
@@ -0,0 +1,28 @@
+#ifndef dk_tool_h
+#define dk_tool_h
+
+typedef struct aNode
+{
+    int value;				
+    struct aNode *next;		
+    struct aNode *prev;		
+}Node;
+
+typedef struct aList
+{			
+	struct aNode *head;		
+	struct aNode *tail;		
+}List;
+
+List *createList();
+
+void createFirstElement(List *aList, int val);				
+void createNextElement(List *aList, int val);			
+
+void printList(List *aList);							
+void deleteList(List *aList);							
+
+void fillElements(List *aList);
+void rePrintList(List *aList);
+
+#endif
