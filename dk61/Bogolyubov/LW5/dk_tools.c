0f6725eff2c9c0eaefed3d184ca38fb33eb3e2d7
LIKIL <likil_thrasher@ukr.net>
2017-06-25 16:59:23 +0300

    Create dk_tools.c

---
+++ dk61/Bogolyubov/LW5/dk_tools.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dk_tools.h"
+
+
+void AddNode(Tree **tagTree, int val) //
+{
+	Tree *now = NULL;
+	if (!(*tagTree)) // esli dereve netu
+	{
+		now = (Tree *)malloc(sizeof(Tree));
+		now->left = now->right = NULL;
+		now->data = val; 
+		*tagTree = now; // uzel
+		return;
+	}
+
+
+	if (val < (*tagTree)->data) //rekursiya
+	{
+		AddNode(&(*tagTree)->left, val); //
+	}
+	else if (val >(*tagTree)->data)
+	{
+		AddNode(&(*tagTree)->right, val);
+	}
+}
+
+void TreePrint(Tree *tagNode)
+{
+	if (tagNode) {
+			
+		TreePrint(tagNode->left); //rekursivij vizov funkcii
+		TreePrint(tagNode->right);// sncachala levo potom pravo
+		printf("%d ", tagNode->data); // posicia printf daet nam ponyat kakoi obhod mi delaem, v danniy moment "ZVOROTNIJ"
+	}
+
+}
+
+Tree *Search(Tree **tagTree, int val) {
+
+	if (!(*tagTree)) {
+		return NULL;
+	}
+
+	if (val == (*tagTree)->data) {
+		return *tagTree;
+	}
+	else if (val < (*tagTree)->data) {
+		Search(&((*tagTree)->left), val);
+	}
+	else if (val >(*tagTree)->data) {
+		Search(&((*tagTree)->right), val);
+	}
+}
+
+
+void DestroyTree(Tree *tagTree) {
+
+	if (tagTree) {
+		DestroyTree(tagTree->left);
+		DestroyTree(tagTree->right);
+		free(tagTree);
+		
+	}
+	//printf("Element destr\n");
+}
