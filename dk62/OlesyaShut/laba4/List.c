5de5c6b9f0d63ae68068f040cd812f5a8714a093
Olesya <olesya.shut@mail.ru>
2017-06-26 07:59:23 +0300

    Update List.c

--- dk62/OlesyaShut/laba4/List.c
+++ dk62/OlesyaShut/laba4/List.c
@@ -5,15 +5,15 @@
 #include <string.h>
 //взяття елементу
 char pop(ptr *top) {
-	if (*top == NULL) //пустий стек
+	if (*top == NULL) 
 		return 0;
-	char value = (*top)->value; //збереження значення
-	ptr tmp = *top; //збереження адреси елементу
-	*top = (*top)->link; //переміщення вказівника вершини стеку на наст. елемент
-	free(tmp); //очистка пам'яті під поточний елемент
+	char value = (*top)->value; 
+	ptr tmp = *top;
+	*top = (*top)->link; 
+	free(tmp);
 	return value; 
 }
-//додавання елементу
+
 void push(ptr *top, char value) {
 	//створення нового елементу
 	ptr tmp = (ptr)malloc(sizeof(node)); 
@@ -25,34 +25,34 @@ void push(ptr *top, char value) {
 }
 //очистка
 void empty(ptr *top) {
-	while (pop(top)); //доки повертається не нуль (нуль - ознака пустого стеку) - виштовшується вершина стеку
+	while (pop(top)); 
 }
-//друк (зворотній)
+
 void print(ptr top) {
-	if (top == NULL) //умова закінчення рекурсії
+	if (top == NULL) 
 		return;
-	print(top->link); //спускаємось до останнього елементу
-	printf("%c",top->value); //на поверненні друкуємо елемент 
+	print(top->link); 
+	printf("%c",top->value);
 }
-//створення "зворотнього" стеку
+
 ptr reverse(ptr top) {
-	if (top == NULL) //пустий стек
+	if (top == NULL) 
 		return NULL;
-	ptr *tmp = (ptr*)malloc(sizeof(ptr)); //вказівник на вершину нового стеку
+	ptr *tmp = (ptr*)malloc(sizeof(ptr)); 
 	*tmp = NULL;
 	char value;
-	while (value = pop(&top)) //перезаписуємо елементи поточного стеку в новий
+	while (value = pop(&top)) 
 		push(tmp,value);
-	return *tmp; //повертаємо вершину нового стеку
+	return *tmp; 
 }
-//стек з масиву символів
+
 void create(ptr *top, char from[]) {
-	int len = strlen(from); //розмір самиву
-	for (int i = 0; i < len; i++) { //додаємо в стек поточний символ
+	int len = strlen(from); 
+	for (int i = 0; i < len; i++) { 
 		push(top,from[i]);
 	}
 }
-//стек з рядка, реазізація аналогічна
+
 void create2(ptr *top, char* from) {
 	if (from == NULL)
 		return;
