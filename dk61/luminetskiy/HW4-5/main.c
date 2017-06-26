3c9b30430db0afdddb32c55b405e1780b4fa3339
romamaka <lumineckiyy447@gmail.com>
2017-06-26 08:02:57 +0300

    Update main.c

--- dk61/luminetskiy/HW4-5/main.c
+++ dk61/luminetskiy/HW4-5/main.c
@@ -4,7 +4,7 @@
 #include "sort.h"
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
+ int main(int argc, char *argv[]) {
     int size;
     printf("Write number of figures: \n");
     scanf("%i",&size);
