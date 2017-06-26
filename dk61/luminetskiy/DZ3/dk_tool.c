6eac0c244a8b93f1a64b413194939807ed20d8ac
romamaka <lumineckiyy447@gmail.com>
2017-06-26 07:58:55 +0300

    Update dk_tool.c

--- dk61/luminetskiy/DZ3/dk_tool.c
+++ dk61/luminetskiy/DZ3/dk_tool.c
@@ -1,4 +1,3 @@
-
 #include "dk_tool.h"
 #include <stdio.h>
 #include <stdbool.h>
@@ -22,30 +21,32 @@ double Squareof(circle s)
 {
     return Pi * s.rad * s.rad ;
 }
-void ShowTheFigures(circle ptr[],int n)
+void ShowTheFigures(circle *ptr, int n)
 {
     for (int k = 0; k < n; k++)
 	{
-        printf("Circle number %i with center(%i,%i), radius %i, and square %i\n",k+1,ptr[k].x,ptr[k].y,ptr[k].rad,ptr[k].square);
+        printf("Circle number %i with center(%i,%i), radius %i, and square %f\n",k+1,ptr[k].x,ptr[k].y,ptr[k].rad,ptr[k].square);
     }
 }
-bool output(char *filename, circle *ptr, int n)
+int output(char *filename, circle *ptr, int n) 
 {
+	if (filename == NULL)
+		return 1;
 	FILE *file = fopen(filename, "w");
 	if (file == NULL)
-		return false;//You don't need to check filename
+		return 1;
 	fprintf(file, "{\n");
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < n; i++) 
 	{
 		fprintf(file,"\t\"Circle %i\": {\n\t\t\"X\": %i,\n\t\t\"Y\": %i,\n\t\t\"Radius\": %i,\n\t\t\"Square\": %.2f\n\t}",\
-			i,ptr[i].x,ptr[i].y,ptr[i].rad,ptr[i].square);// Ð¼Ð°Ñ‚ÑŽÐºÐ°Ñ”Ñ‚ÑŒÑÑ Ð½Ð° square
+			i,ptr[i].x,ptr[i].y,ptr[i].rad, Squareof(ptr[i]));
 		if (i != n - 1)
 			fprintf(file, ",");
-        fflush(file);
 		fprintf(file, "\n");
-
+		
 	}
 	fprintf(file,"}\n");
+	fflush(file);
 	fclose(file);
 	return 0;
 }
