8d4e283396dab5ccee86d8304e245f809356d02b
Olesya <olesya.shut@mail.ru>
2017-06-26 07:54:38 +0300

    Update Main.c

--- dk62/OlesyaShut/laba4/Main.c
+++ dk62/OlesyaShut/laba4/Main.c
@@ -2,11 +2,10 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-//максимальна довжина рядка для обробки
 #define LEN 255
 
 
-//читання цілого числа з клавіатури
+
 int readInt() {
 	char in;
 	scanf("%d",&in);
@@ -14,39 +13,39 @@ int readInt() {
 }
 
 int main() {
-	int choice; //обраний пункт меню
-	ptr top = NULL; //вказівник на вершину стеку
-	char *str; //рядок, що обробляється
+	int choice; 
+	ptr top = NULL; 
+	char *str;
 	do {
 		printf("\n1: Append to list\n2: Print list\n3: Process\n4: Delete list\n5: Exit\n");
 		choice = readInt();
 		switch(choice) {
 			case 1:
-				//введення рядка
+				
 				str = (char*)malloc(LEN+1);
 				printf("Type line to check (no more than %d characters): ",LEN);
 				getc(stdin);
 				fgets(str,LEN,stdin);
 				
-				create(&top,str); //додавання рядка до стеку
-				free(str); //очистка пам'яті під рядок
+				create(&top,str); 
+				free(str); 
 				break;
 			case 2:
-				//друк
+				
 				print(top);
 				printf("\n");
 				break;
 			case 3:
-				show(process(reverse(top))); //обробка рядка (автоматично очищує стек)
-				top = NULL; //гарантія коректної роботи
+				show(process(reverse(top))); 
+				top = NULL; 
 				break;
 			case 4:
 				printf("Deleting list\n");
-				empty(&top); //очистка стеку
-				top = NULL; //гарантія коректної роботи
+				empty(&top); 
+				top = NULL; 
 				break;
 			case 5:
-				empty(&top); //очистка стеку перед завершенням програми
+				empty(&top); 
 				break;
 			default:
 				printf("Wrong command\n");
