9e30c6261afb7c3ce66b2527749ad2fb1a1a3883
AShashkov <andr-sh@ukr.net>
2017-06-26 09:47:31 +0300

    Add files via upload

--- dk62/Andrey_Shashkov/Laba3/dk_tool.h
+++ dk62/Andrey_Shashkov/Laba3/dk_tool.h
@@ -3,6 +3,6 @@
 
 char *Transform_file (int *Lenght_of_Str);
 void shellSort(int Lenght_of_Str ,char *string);
-unsigned binarySearch(int Lenght_of_Str ,char *string, char Key);
+unsigned lineSearch(int Lenght_of_Str ,char *string, char Key);
 
 #endif
