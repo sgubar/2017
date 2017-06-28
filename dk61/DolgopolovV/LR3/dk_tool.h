05332a492aa4b863d04137633165ce69534e999d
hogl030 <hogl030@gmail.com>
2017-06-28 23:01:34 +0300

    Create dk_tool.h

---
+++ dk61/DolgopolovV/LR3/dk_tool.h
@@ -0,0 +1,13 @@
+#ifndef dk_tool
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define dk_tool
+
+#define Max 10000
+char *sort(char *mas);
+char *sort_1(char *mas);
+char *open(void);
+void save(char text);
+
+#endif
