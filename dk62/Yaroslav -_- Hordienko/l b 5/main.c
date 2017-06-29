b33dcfc2fc2aa4c8c2be4957d6585deec444bf49
Goordey <Goordey1998@gmail.com>
2017-06-29 07:39:27 +0300

    Update main.c

--- dk62/Yaroslav -_- Hordienko/l b 5/main.c
+++ dk62/Yaroslav -_- Hordienko/l b 5/main.c
@@ -4,42 +4,70 @@
  *  Created on: 16 ìàÿ 2017 ã.
  *      Author: Yaroslav Hordiienko
  */
-
+#include "Tree.h"
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "tool.h"
+#include <time.h>
+
+
+int readInt() {
+	int in;
+	scanf("%d",&in);
+	return in;
+}
 
 int main()
 {
-    Node *head = NULL;
-    Node *last = NULL;
-    CreateList(&head, &last);
-
-
-    for(int i = 0; i < 15; i++)
-    {
-        AddElementToList(&head, &last);
-    }
-
-    Node *tmpHead = head;
-    head = head->next;
-    free(tmpHead);
-
-    PrintElements(&head, &last);
-
-    Node *tmp = FindMinimum(&head, &last);
-
-    Sort(&head, &last, &tmp);
-
-    PrintElements(&head, &last);
-
-    for(int i = 0; i < 15; i++)
-    {
-        DeleteElement(&head);
-    }
-
-    system("pause");
-
-    return 0;
+	srand(time(NULL));
+	int choice; 
+	ptr root = NULL;
+	do {
+		printf("\n1: Create tree\n2: Print tree\n3: Add node\n4: Find node\n5: Remove node\n6: Delete tree\n7: Exit\n");
+		choice = readInt();
+		switch(choice) {
+			case 1:
+				printf("Tree size: ");
+				if (root) 
+					erase(&root);
+				root = create(readInt()); 
+				break;
+			case 2:
+				print(root,0);
+				break;
+			case 3:
+				printf("Node value (integer value, except zero): ");
+				
+				printf("%s\n", add(&root,readInt()) ? "Creating new node" : "Replacing node"); 
+				break;
+			case 4:
+				printf("Node value (integer value, except zero): ");
+				
+				printf("%s\n", find(root,readInt()) ? "Element exists" : "No node found");
+				break;
+			case 5:
+				printf("Node value (integer value, except zero): ");
+				int value = readInt();
+				if (find(root,value) == NULL) 
+					printf("No element to remove\n");
+				else {
+					rem(&root,value);
+					printf("Removing node\n");
+				}
+				break;
+			case 6:
+				
+				printf("Deleting tree\n");
+				erase(&root);
+				root = NULL;
+				break;
+			case 7:
+				erase(&root);
+				break;
+			default:
+				printf("Wrong command\n");
+				break;
+		}
+	} while (choice != 7);
+	return 0;
 }
